4441716a7db0c6c57f1a3332baa43fd4121973a2
Ezra Zygmuntowicz <ez@vmware.com>
2011-05-13 01:04:34 -0700

    Add Rack support as a first class citizen framework
    Added rack staging pliugin, rack apps require a config.ru
    
    and a Gemfile to run, this is a stake in the ground to make
    
    life easier for rack apps and to enforce best practices
    
    Change-Id: Ie735c8b3fc628a8836b31b28bfacbacd846f1cca

---
+++ cloud_controller/staging/rack/plugin.rb
@@ -0,0 +1,54 @@
+class RackPlugin < StagingPlugin
+  include GemfileSupport
+  def framework
+    'rack'
+  end
+
+  def stage_application
+    Dir.chdir(destination_directory) do
+      create_app_directories
+      copy_source_files
+      compile_gems
+      create_startup_script
+    end
+  end
+
+  # Rack has a standard startup process.
+  def start_command
+    if ! File.exist?("app/Gemfile")
+      raise "Rack applications *must* have a Gemfile"
+    end
+    "#{local_runtime} -S bundle exec #{gem_bin_dir}/thin -R config.ru $@ start"
+  end
+
+  private
+
+  # Returns a path relative to the 'app' directory.
+  def gem_bin_dir
+    "./rubygems/ruby/#{library_version}/bin"
+  end
+
+  def startup_script
+    vars = environment_hash
+    if uses_bundler?
+      vars['PATH'] = "$PWD/app/rubygems/ruby/#{library_version}/bin:$PATH"
+      vars['GEM_PATH'] = vars['GEM_HOME'] = "$PWD/app/rubygems/ruby/#{library_version}"
+    else
+      vars['RUBYOPT'] = "-rubygems -I$PWD/ruby -rstdsync"
+    end
+    # PWD here is after we change to the 'app' directory.
+    generate_startup_script(vars) do
+      plugin_specific_startup
+    end
+  end
+
+  def plugin_specific_startup
+    cmds = []
+    unless uses_bundler?
+      cmds << "mkdir ruby"
+      cmds << 'echo "\$stdout.sync = true" >> ./ruby/stdsync.rb'
+    end
+    cmds.join("\n")
+  end
+
+end
