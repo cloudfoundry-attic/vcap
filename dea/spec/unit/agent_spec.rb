4fe9a0b7b137288a9a96606bc7b4e399abe04db3
mpage <mpage@vmware.com>
2011-05-13 11:38:24 -0700

    Remove instance directories for untracked droplets. Remove instance dirs for crashed droplets when we delete the associated entry in the droplets hash.
    Change-Id: I2d18f7079167cafa1875b23de843ba3360c79db8

--- dea/spec/unit/agent_spec.rb
+++ dea/spec/unit/agent_spec.rb
@@ -35,15 +35,98 @@ describe 'DEA Agent' do
         'intervals' => { 'heartbeat' => 1 },
         'base_dir'  => UNIT_TESTS_DIR
       )
-      apps_dir = File.join(UNIT_TESTS_DIR, 'apps')
-      FileUtils.mkdir(apps_dir)
+      apps_dir = create_apps_dir(UNIT_TESTS_DIR)
       File.directory?(apps_dir).should be_true
 
       du_pid = agent.dump_apps_dir
-      Process.waitpid(du_pid)
+      begin
+        Process.waitpid(du_pid)
+      rescue Errno::ECHILD
+        # The agent detaches du_pid, consequently, the ruby thread that was set up
+        # to reap the child status may have already done so.
+      end
 
       logs = Dir.glob(File.join(UNIT_TESTS_DIR, 'apps.du.*'))
       logs.length.should == 2
     end
+  end
+
+  describe '#delete_untracked_instance_dirs' do
+    it 'should not remove instance dirs for tracked apps' do
+      agent = DEA::Agent.new(
+        'intervals' => { 'heartbeat' => 1 },
+        'base_dir'  => UNIT_TESTS_DIR
+      )
+      apps_dir = create_apps_dir(UNIT_TESTS_DIR)
+      File.directory?(apps_dir).should be_true
+
+      inst_dir = File.join(apps_dir, 'test_instance_dir')
+      FileUtils.mkdir(inst_dir)
+      File.directory?(inst_dir).should be_true
+
+      agent.instance_variable_set(:@droplets, {1 => {0 => {:dir => inst_dir}}})
+      agent.delete_untracked_instance_dirs
+
+      File.directory?(inst_dir).should be_true
+    end
+
+    it 'should remove instance dirs for untracked apps' do
+      agent = DEA::Agent.new(
+        'intervals' => { 'heartbeat' => 1 },
+        'base_dir'  => UNIT_TESTS_DIR
+      )
+      apps_dir = create_apps_dir(UNIT_TESTS_DIR)
+      File.directory?(apps_dir).should be_true
+
+      inst_dir = File.join(apps_dir, 'test_instance_dir')
+      FileUtils.mkdir(inst_dir)
+      File.directory?(inst_dir).should be_true
+
+      agent.instance_variable_set(:@logger, double(:logger).as_null_object)
+      agent.instance_variable_set(:@droplets, {})
+      agent.delete_untracked_instance_dirs
+
+      File.directory?(inst_dir).should be_false
+    end
+  end
+
+  describe '#crashes_reaper' do
+    it 'should remove instance dirs for crashed apps' do
+      agent = DEA::Agent.new(
+        'intervals' => { 'heartbeat' => 1 },
+        'base_dir'  => UNIT_TESTS_DIR
+      )
+      apps_dir = create_apps_dir(UNIT_TESTS_DIR)
+      File.directory?(apps_dir).should be_true
+
+      inst_dir = File.join(apps_dir, 'test_instance_dir')
+      FileUtils.mkdir(inst_dir)
+      File.directory?(inst_dir).should be_true
+
+      droplets = {
+        0 => {
+          0 => {
+            :dir   => inst_dir,
+            :state => :CRASHED,
+            :state_timestamp => Time.now.to_i - DEA::Agent::CRASHES_REAPER_TIMEOUT - 60,
+          },
+        }
+      }
+      agent.instance_variable_set(:@logger, double(:logger).as_null_object)
+      agent.instance_variable_set(:@droplets, droplets)
+
+      EM.run do
+        agent.crashes_reaper
+        EM.stop
+      end
+
+      File.directory?(inst_dir).should be_false
+    end
+  end
+
+  def create_apps_dir(base_dir)
+    apps_dir = File.join(base_dir, 'apps')
+    FileUtils.mkdir(apps_dir)
+    apps_dir
   end
 end
