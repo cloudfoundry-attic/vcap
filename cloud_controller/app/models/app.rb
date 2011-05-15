5995ee3eb8988c4b16d12b2b6e7c8e5d74dcc04d
Gareth Rushgrove <gareth@morethanseven.net>
2011-05-15 17:44:07 +0100

    Add support for Python WSGI applications

--- cloud_controller/app/models/app.rb
+++ cloud_controller/app/models/app.rb
@@ -19,8 +19,8 @@ class App < ActiveRecord::Base
 
   AppStates = %w[STOPPED STARTED]
   PackageStates = %w[PENDING STAGED FAILED]
-  Runtimes = %w[ruby18 ruby19 java node erlangR14B02]
-  Frameworks = %w[sinatra rails3 spring grails node otp_rebar unknown]
+  Runtimes = %w[ruby18 ruby19 java node erlangR14B02 python26]
+  Frameworks = %w[sinatra rails3 spring grails node otp_rebar wsgi unknown]
 
   validates_presence_of :name, :framework, :runtime
 
---
+++ cloud_controller/staging/wsgi/plugin.rb
@@ -0,0 +1,33 @@
+class WsgiPlugin < StagingPlugin
+  def framework
+    'wsgi'
+  end
+
+  def stage_application
+    Dir.chdir(destination_directory) do
+      create_app_directories
+      copy_source_files
+      create_startup_script
+      create_gunicorn_config
+    end
+  end
+
+  def start_command
+    "gunicorn -c ../gunicorn.config app:app"
+  end
+
+  private
+  def startup_script
+    vars = environment_hash
+    generate_startup_script(vars)
+  end
+
+  def create_gunicorn_config
+    File.open('gunicorn.config', 'w') do |f|
+      f.write <<-EOT
+import os
+bind = "127.0.0.1:%s" % os.environ['VMC_APP_PORT']
+      EOT
+    end
+  end
+end
