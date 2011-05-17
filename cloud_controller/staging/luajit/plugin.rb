da717f2b884ebdf9001604a6ee3a411c4d517dde
Ezra Zygmuntowicz <ez@vmware.com>
2011-05-16 18:27:23 -0700

    massaging CGILua config files trying to make .lua and .lp work

--- cloud_controller/staging/luajit/plugin.rb
+++ cloud_controller/staging/luajit/plugin.rb
@@ -18,7 +18,19 @@ class LuajitPlugin < StagingPlugin
   private
   def startup_script
     vars = environment_hash
-    generate_startup_script(vars)
+    vars['CGILUA_CONF'] = "$PWD/luaconf"
+    generate_startup_script(vars) do
+      create_cgilua_conf
+    end
+  end
+  
+  def create_cgilua_conf
+    cnf = []
+    cnf << "mkdir luaconf"
+    cnf << %Q{echo "cgilua.addopenfunction (cgilua.htmlheader)" >> luaconf/config.lua}
+    cnf << %Q{echo "cgilua.addscripthandler ('.lua', cgilua.doscript)" >> luaconf/config.lua}
+    cnf << %Q{echo "cgilua.addscripthandler ('.lp', cgilua.handlelp)" >> luaconf/config.lua}
+    cnf.join("\n")
   end
 
 end
