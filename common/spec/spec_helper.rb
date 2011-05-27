d660e2f4472233432a794e432afddc5fe77d72be
Vadim Spivak <vspivak@vmware.com>
2011-05-26 18:15:17 -0700

    add rolling metric
    Change-Id: Ifef3228ca90f183e40da6204b13c91cc4f5cd50a

--- common/spec/spec_helper.rb
+++ common/spec/spec_helper.rb
@@ -2,6 +2,8 @@
 $:.unshift File.expand_path("../../lib", __FILE__)
 require "bundler"
 Bundler.require(:default, :spec)
+
 require "vcap/common"
 require "vcap/component"
+require "vcap/rolling_metric"
 require "vcap/json_schema"
