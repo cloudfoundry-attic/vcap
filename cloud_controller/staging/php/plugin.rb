00d4b207906d1048b85c51c9e4975497c16e7f00
Lucas Carlson <lucas@rufy.com>
2011-07-08 12:01:59 -0700

    Support for deploying PHP applications through a standard Apache configuration with built-in support for APC, memcache, mongo and redis.
    Change-Id: I2d2bc61bc289a0bc7a92eb1c8e5c3f41375437f1

---
+++ cloud_controller/staging/php/plugin.rb
@@ -0,0 +1,41 @@
+require File.expand_path('../../apache_common/apache', __FILE__)
+
+class PhpPlugin < StagingPlugin
+  def framework
+    'php'
+  end
+
+  def stage_application
+    Dir.chdir(destination_directory) do
+      create_app_directories
+      Apache.prepare(destination_directory)
+      copy_source_files
+      create_startup_script
+    end
+  end
+
+  # The Apache start script runs from the root of the staged application.
+  def change_directory_for_start
+    "cd apache"
+  end
+
+  def start_command
+    "bash ./start.sh"
+  end
+
+  private
+
+  def startup_script
+    vars = environment_hash
+    generate_startup_script(vars) do
+      <<-PHPEOF
+env > env.log
+ruby resources/generate_apache_conf $VCAP_APP_PORT $HOME $VCAP_SERVICES
+      PHPEOF
+    end
+  end
+
+  def apache_server_root
+    File.join(destination_directory, 'apache')
+  end
+end
