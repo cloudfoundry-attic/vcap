d0fb01f59b9a59bfba80755c546b8b2fd97d103c
mpage <mpage@vmware.com>
2011-07-20 19:10:45 -0700

    Refactor stager tests
    Summary:
    - Fix the warning for multiple definitions of AUTOSTAGING_JAR
    - Move the embedded startup scripts into the fixture directory (lays the groundwork for fully staged droplets)
    - General code cleanup
    
    Test plan:
    - Unit tests still pass
    
    Change-Id: I00c8e6246608abc905f4add093592ab887154469

--- stager/spec/functional/lift_spec.rb
+++ stager/spec/functional/lift_spec.rb
@@ -3,38 +3,37 @@
 
 require 'spec_helper'
 
-AUTOSTAGING_JAR = 'auto-reconfiguration-0.6.0-BUILD-SNAPSHOT.jar'
 LIFT_FILTER_CLASS = 'net.liftweb.http.LiftFilter'
 CF_LIFT_PROPERTIES_GENERATOR_CLASS =
   'org.cloudfoundry.reconfiguration.CloudLiftServicesPropertiesGenerator';
 
 describe "A Lift application being staged without a web.xml in its web config will be rejected" do
   before(:all) do
-    app_fixture :lift_no_web_config
+    @app_fix =  VCAP::Stager::Spec::JavaAppFixture.new(:lift_no_web_config)
   end
 
   it "should be fail the staging" do
-    lambda { stage :lift }.should raise_error("Scala / Lift application staging failed: web.xml not found")
+    lambda { @app_fix.stage :lift }.should raise_error("Scala / Lift application staging failed: web.xml not found")
   end
 end
 
 describe "A Lift application being staged without a LiftFilter in its web config will be rejected" do
   before(:all) do
-    app_fixture :lift_no_lift_filter
+    @app_fix = VCAP::Stager::Spec::JavaAppFixture.new(:lift_no_lift_filter)
   end
 
   it "should fail the staging" do
-    lambda { stage :lift }.should raise_error("Scala / Lift application staging failed: no LiftFilter class found in web.xml")
+    lambda { @app_fix.stage :lift }.should raise_error("Scala / Lift application staging failed: no LiftFilter class found in web.xml")
   end
 end
 
 describe "A Lift application with only a LiftFilter being staged will contain a CloudLiftServicesPropertiesGenerator in its web config after staging" do
   before(:all) do
-    app_fixture :lift_simple
+    @app_fix = VCAP::Stager::Spec::JavaAppFixture.new(:lift_simple)
   end
 
   it "should contain a CloudLiftServicesPropertiesGenerator ServletContextListener in its web config" do
-    stage :lift do |staged_dir|
+    @app_fix.stage :lift do |staged_dir|
       web_config_file = File.join(staged_dir, 'tomcat/webapps/ROOT/WEB-INF/web.xml')
       File.exist?(web_config_file).should == true
 
@@ -48,7 +47,7 @@ describe "A Lift application with only a LiftFilter being staged will contain a 
   end
 
   it "should contain the CloudLiftServicesPropertiesGenerator ServletContextListener before the LiftFilter in its web config" do
-    stage :lift do |staged_dir|
+    @app_fix.stage :lift do |staged_dir|
       web_config_file = File.join(staged_dir, 'tomcat/webapps/ROOT/WEB-INF/web.xml')
       File.exist?(web_config_file).should == true
 
@@ -70,7 +69,7 @@ describe "A Lift application with only a LiftFilter being staged will contain a 
   end
 
   it "should have the auto reconfiguration jar in the webapp lib path" do
-    stage :lift do |staged_dir|
+    @app_fix.stage :lift do |staged_dir|
       auto_reconfig_jar_relative_path = "tomcat/webapps/ROOT/WEB-INF/lib/#{AUTOSTAGING_JAR}"
       auto_reconfiguration_jar_path = File.join(staged_dir, auto_reconfig_jar_relative_path)
       File.exist?(auto_reconfiguration_jar_path).should == true
@@ -81,11 +80,11 @@ end
 
 describe "A Lift application with a servlet and a LiftFilter being staged will contain a CloudLiftServicesPropertiesGenerator in its web config after staging" do
   before(:all) do
-    app_fixture :lift_simple_servlet
+    @app_fix = VCAP::Stager::Spec::JavaAppFixture.new(:lift_simple_servlet)
   end
 
   it "should contain a CloudLiftServicesPropertiesGenerator ServletContextListener in its web config" do
-    stage :lift do |staged_dir|
+    @app_fix.stage :lift do |staged_dir|
       web_config_file = File.join(staged_dir, 'tomcat/webapps/ROOT/WEB-INF/web.xml')
       File.exist?(web_config_file).should == true
 
@@ -99,7 +98,7 @@ describe "A Lift application with a servlet and a LiftFilter being staged will c
   end
 
   it "should contain the CloudLiftServicesPropertiesGenerator ServletContextListener before the servlet in its web config" do
-    stage :lift do |staged_dir|
+    @app_fix.stage :lift do |staged_dir|
       web_config_file = File.join(staged_dir, 'tomcat/webapps/ROOT/WEB-INF/web.xml')
       File.exist?(web_config_file).should == true
 
@@ -118,7 +117,7 @@ describe "A Lift application with a servlet and a LiftFilter being staged will c
   end
 
   it "should have the auto reconfiguration jar in the webapp lib path" do
-    stage :lift do |staged_dir|
+    @app_fix.stage :lift do |staged_dir|
       auto_reconfig_jar_relative_path = "tomcat/webapps/ROOT/WEB-INF/lib/#{AUTOSTAGING_JAR}"
       auto_reconfiguration_jar_path = File.join(staged_dir, auto_reconfig_jar_relative_path)
       File.exist?(auto_reconfiguration_jar_path).should == true
---
+++ stager/spec/support/java_staging_helpers.rb
@@ -0,0 +1,1 @@
+AUTOSTAGING_JAR = 'auto-reconfiguration-0.6.0-BUILD-SNAPSHOT.jar'
