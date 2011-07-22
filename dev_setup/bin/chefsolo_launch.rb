d3d9f145dd63db3df462cb6e8807a32c8c521f16
Mahesh <mpatil@vmware.com>
2011-07-22 11:27:55 -0700

    Adhere to our Ruby coding convention
    plus one minor fix to handle both lowercase and uppercase proxy env variables
    
    Change-Id: I96d0f6db5291ae6124aecf83d01ff3a07e4127d7

--- dev_setup/bin/chefsolo_launch.rb
+++ dev_setup/bin/chefsolo_launch.rb
@@ -61,7 +61,7 @@ end
 `chown #{deployment_user} #{deployment_home} #{deployment_home}/deploy #{deployment_home}/sys/log`
 puts "Installing deployment #{deployment_name}, deployment home dir is #{deployment_home}, vcap dir is #{vcap_dir}"
 
-run_list = Array.new
+run_list = []
 run_list << "role[dea]"
 run_list << "role[router]"
 run_list << "role[cc]"
@@ -71,9 +71,9 @@ run_list << "role[cloudfoundry]" if download_cloudfoundry
 env = env_erb.result
 
 # Deploy all the cf components
-Dir.mktmpdir { |tmpdir|
+Dir.mktmpdir do |tmpdir|
   # Create chef-solo config file
-  File.open("#{tmpdir}/solo.rb", "w") { |f|
+  File.open("#{tmpdir}/solo.rb", "w") do |f|
     f.puts("cookbook_path \"#{File.expand_path("../cookbooks", script_dir)}\"")
     f.puts("role_path \"#{File.expand_path("../roles", script_dir)}\"")
 
@@ -90,48 +90,53 @@ Dir.mktmpdir { |tmpdir|
     unless ENV["no_proxy"].nil?
         f.puts("no_proxy \"#{ENV["no_proxy"]}\"")
     end
-  }
-
+  end
 
   # Create chef-solo attributes file
   json_attribs = "#{tmpdir}/solo.json"
-  File.open(json_attribs, "w") { |f|
-    f.puts(env)
-  }
+  File.open(json_attribs, "w") {|f| f.puts(env)}
 
   FileUtils.cp("#{tmpdir}/solo.rb", "/tmp/solo.rb")
   FileUtils.cp("#{json_attribs}", "/tmp/solo.json")
 
-  id = fork {
-    proxy_env = Array.new
-    proxy_env << "http_proxy=#{ENV["http_proxy"]}" unless ENV["http_proxy"].nil?
-    proxy_env << "https_proxy=#{ENV["https_proxy"]}" unless ENV["https_proxy"].nil?
-    proxy_env << "no_proxy=#{ENV["no_proxy"]}" unless ENV["no_proxy"].nil?
-    exec("sudo env #{proxy_env.join(" ")} chef-solo -c #{tmpdir}/solo.rb -j #{json_attribs} -l debug")
-  }
-  pid, status = Process.waitpid2(id)
-  if status.exitstatus == 0
-    # save the config of this deployment
-    config_base_dir = File.expand_path(File.join(vcap_dir, CONFIG_BASE_DIR))
-    FileUtils.mkdir_p(config_base_dir)
-    `chown #{deployment_user} #{config_base_dir}`
-
-    config_dir = File.join(config_base_dir, deployment_name)
-    FileUtils.mkdir_p(config_dir)
-    `chown #{deployment_user} #{config_dir}`
-
-    File.open(File.join(config_dir, DEPLOYMENT_FILE), "w") { |f|
-      f.puts(env)
-    }
-
-    puts "Cloudfoundry setup was successful."
-    if deployment_name != DEFAULT_DEPLOYMENT_NAME
-      puts "Config file for this deployment is in #{config_dir}."
+  id = fork do
+    proxy_env = []
+    # Setup proxy
+    %w(http_proxy https_proxy no_proxy).each do |env_var|
+      if  !ENV[env_var].nil? || !ENV[env_var.upcase].nil?
+        [env_var, env_var.upcase].each do |v|
+          proxy = "#{v}=#{ENV[v.downcase] || ENV[v.upcase]}"
+          proxy_env << proxy
+        end
+      end
     end
-
-    env = JSON.parse(env)
-    ruby_path = "#{env["ruby"]["path"]}/bin"
-    puts "Note: You may need to add #{ruby_path} to your path to use vmc"
+    exec("sudo env #{proxy_env.join(" ")} chef-solo -c #{tmpdir}/solo.rb -j #{json_attribs} -l debug")
   end
-  exit status.exitstatus
-}
+
+  pid, status = Process.waitpid2(id)
+  if status.exitstatus != 0
+    exit status.exitstatus
+  end
+
+  # save the config of this deployment
+  config_base_dir = File.expand_path(File.join(vcap_dir, CONFIG_BASE_DIR))
+  FileUtils.mkdir_p(config_base_dir)
+  `chown #{deployment_user} #{config_base_dir}`
+
+  config_dir = File.join(config_base_dir, deployment_name)
+  FileUtils.mkdir_p(config_dir)
+  `chown #{deployment_user} #{config_dir}`
+
+  File.open(File.join(config_dir, DEPLOYMENT_FILE), "w") { |f|
+    f.puts(env)
+  }
+
+  puts "Cloudfoundry setup was successful."
+  if deployment_name != DEFAULT_DEPLOYMENT_NAME
+    puts "Config file for this deployment is in #{config_dir}."
+  end
+
+  env = JSON.parse(env)
+  ruby_path = "#{env["ruby"]["path"]}/bin"
+  puts "Note: You may need to add #{ruby_path} to your path to use vmc"
+end
