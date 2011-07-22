b088a41bcf9e89ba4a780f5dce71c8366fd223d2
Mahesh <mpatil@vmware.com>
2011-07-22 14:50:57 -0700

    More license fixes
    Change-Id: I74502d897bce163486755662a39fc3ce733030f5

--- dev_setup/cookbooks/ruby/metadata.rb
+++ dev_setup/cookbooks/ruby/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "VMware"
-maintainer_email "ac-eng@vmware.com"
-license          "N/A"
+maintainer_email "support@vmware.com"
+license          "Apache 2.0"
 description      "Installs/Configures ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0.0"
