5fbaceb589e104b26c052cfb8e76ce7b937847cb
Yohei Saskai <yohei.sasaki@mail.rakuten.com>
2011-08-05 12:55:23 +0900

    fix File.expand_path usage.
    For applying this fix, you can use MySQL (/w em_mysql2 adapter) as cloud controller database by configuring cloud_controller.yml as follows:
    
      development:
        database: cloudcontroller
        host: localhost
        port: 3306
        username: root
        password: password
        adapter: em_mysql2
        encoding: utf8
        timeout: 2000
    
    Change-Id: Ie881f617f77d1d4aacc0772b1594547b9beb1af5

--- cloud_controller/vendor/plugins/mysql_fiber_patch/init.rb
+++ cloud_controller/vendor/plugins/mysql_fiber_patch/init.rb
@@ -1,6 +1,6 @@
 db_config = Rails.application.config.database_configuration[::Rails.env]
 if db_config['adapter'] == 'em_mysql2'
-  require File.expand_path('active_record_fiber_patches', __FILE__)
+  require File.expand_path('../active_record_fiber_patches', __FILE__)
   ActiveRecord::ConnectionAdapters.register_fiber_pool(CloudController::UTILITY_FIBER_POOL)
 end
 
