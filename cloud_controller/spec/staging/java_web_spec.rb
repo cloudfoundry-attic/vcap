9ffed1f682f73b81f5d3c559c176b9336d0cfc9c
AB Srinivasan <abyss@b20nine.com>
2011-08-05 10:21:47 -0700

    Support for Plain Java and refactored Java frameworks staging.
    Change-Id: Id0e9aa95cf65aae88e815e9c701d65a7e06fad7f

---
+++ cloud_controller/spec/staging/java_web_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "A Java web application being staged without a web config" do
+  before do
+    app_fixture :java_web_no_web_config
+  end
+
+  it "should fail" do
+    lambda { stage :java_web }.should raise_error
+  end
+end
+
+describe "A Java web being staged " do
+  before(:all) do
+    app_fixture :java_web
+  end
+
+  it "should not be modified during staging" do
+    stage :java_web do |staged_dir, source_dir|
+      source_app_files = Dir.glob("#{source_dir}/**/*", File::FNM_DOTMATCH)
+      staged_app_root = File.join(staged_dir, 'tomcat/webapps/ROOT')
+      staged_app_files = Dir.glob("#{staged_app_root}/**/*", File::FNM_DOTMATCH)
+      source_app_files.should_not == nil
+      staged_app_files.should_not == nil
+      source_app_files.length.should == staged_app_files.length
+      source_app_files.each do |filename|
+        next if (File.directory?(filename) || !File.exists?(filename))
+        staged_app_file = filename.sub(/#{source_dir}/, "#{staged_app_root}")
+        File.exists?(staged_app_file).should == true
+        File.size(filename).should == File.size(staged_app_file)
+        Digest::SHA1.file(filename).hexdigest.should ==  Digest::SHA1.file(staged_app_file).hexdigest
+      end
+    end
+  end
+
+  it "should not have the auto reconfiguration jar in the webapp lib path" do
+    stage :java_web do |staged_dir|
+      auto_reconfig_jar_relative_path = "tomcat/webapps/ROOT/WEB-INF/lib/#{AUTOSTAGING_JAR}"
+      auto_reconfiguration_jar_path = File.join(staged_dir, auto_reconfig_jar_relative_path)
+      File.exist?(auto_reconfiguration_jar_path).should == false
+    end
+  end
+
+end
