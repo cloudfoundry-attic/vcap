a8e24ccb641e45682f5d7a573ae724826a85c9ed
mpage <mpage@vmware.com>
2011-08-12 16:39:01 -0700

    Pull existing staging code out of the CC into its own gem
    Change-Id: Ia33f0c7b09b98dca076ea1ed461eb6ff390dd313

--- cloud_controller/spec/fixtures/apps/rails3_nodb/source/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rails3Nodb::Application.config.secret_token = '796a1a08531242fe299326f17d8f68c96355e138074ed9074c9700b7a45f32ac380a83bf4bc7b0caaf00ebb66f41e329ad6615b1f1a5993989efd0eef7c4855b'
--- cloud_controller/staging/README
+++
@@ -1,7 +0,0 @@
-=== Staging Overview
-
-=== Directory Structure
-
-=== Implementing a Staging Plugin in Ruby
-
-=== Implementing a Staging Plugin in Something Else
