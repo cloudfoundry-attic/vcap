2d0873752ff4a40d9b2f5149cca90208ce4676e0
Mahesh <mpatil@vmware.com>
2011-08-15 09:56:58 -0700

    Multi host part 2 + changes to allow deployment like bash < <(curl vcap_dev_setup_url)
    Change
      This review has changes related to the following 3 things.
      1. Move all the default config (that was embeded in
          vcap_dev/vcap_dev_setup/job_manager) into chef cookbooks.
      2. Add postgres CCDB.
      3. Multi host setup for services and dea. This meant separating service
      gateway from service nodes. To this effect we added a service gateway role.
    
    Overall a lot of files have changed, most of those changes are related to
    moving all the default config values from the various wrapper scripts to the
    cookbooks, especially the new cookbook called deployment which is the holding
    place for all the deployment related config options.
    
    * dev_setup/bin/vcap:
      Added vcap to dev_setup/bin instead of just changing bin/vcap. The reason for
      this is that opensource CF code will not be in sync with the
      private repo and internal testing of CF dev_setup scrips would not work if it
      relied on the version of vcap in the opensource repo i.e. bin/vcap. So now we
      just package vcap with dev_setup.
    
    * vcap_dev
      * Since all defaults are now maintained in chef scripts. The chef "deployment"
      * role/recipe creates a deployment info file that is consumed by this vcap_dev
      * script.
      * The vcap_dev_setup file now saves the list of components that were installed
        for a deployment. This script only starts the components included in that
        list.
      * Uses the vcap binary from dev_setup/bin
    
    * vcap_dev_setup
      Moved a bunch of directory creating code to the deployment cookbook
    
    * Added a CCDB role/recipe.
      * Creates and Configures the CC postgres database.
    
    * Added deployment cookbook
      * Moves all the comon directory creation code here. Note, we should go back
        and see if we really do need all these various directories and probably come
        up with a well designed directory layout.
    
    Testing:
    * Ran BVTS. Lift BVT failure needs investigating (I will fix/get to the bottom
      of it before i submit this change)
    * Tested the following multi host setup.
      1. Ran mysql database with mysql node on 2 VMs. On a 3rd VM ran everything
      else. Verified that BVTs passed.
      2. Ran dea on 2 different VMs. Ran everything else on a 3rd VM. Verified
      that BVTs passed.
    
        Note: I have only tested the multihost setup with mysql. I will test mongodb
        and redis before submitting this change, I expect those to run without any
        glitches.
    
    Change-Id: I6a084be09a81bf920eebc62be8d7aa6625cc17e9

--- dev_setup/cookbooks/postgresql/recipes/default.rb
+++ dev_setup/cookbooks/postgresql/recipes/default.rb
@@ -5,14 +5,31 @@
 # Copyright 2011, VMware
 #
 #
-
+#
 %w[libpq-dev postgresql].each do |pkg|
   package pkg
 end
 
-bash "Setup PostgreSQL" do
-  user "postgres"
-  code <<-EOH
-  /usr/bin/psql -c "alter role postgres password '#{node[:postgresql][:server_root_password]}'"
-  EOH
+case node['platform']
+when "ubuntu"
+  ruby_block "postgresql_conf_update" do
+    block do
+      / \d*.\d*/ =~ `pg_config --version`
+      pg_major_version = $&.strip
+
+      # update postgresql.conf
+      postgresql_conf_file = File.join("", "etc", "postgresql", pg_major_version, "main", "postgresql.conf")
+      `grep "^\s*listen_addresses" #{postgresql_conf_file}`
+      if $?.exitstatus != 0
+        `echo "listen_addresses='#{node[:postgresql][:host]},localhost'" >> #{postgresql_conf_file}`
+      else
+        `sed -i.bkup -e "s/^\s*listen_addresses.*$/listen_addresses='#{node[:postgresql][:host]},localhost'/" #{postgresql_conf_file}`
+      end
+
+      # Cant use service resource as service name needs to be statically defined
+      `/etc/init.d/postgresql-#{pg_major_version} restart`
+    end
+  end
+else
+  Chef::Log.error("Installation of postgres is not supported on this platform.")
 end
