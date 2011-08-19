8819e3ef05dafbd63af09928481c943d80f9032f
Tal Garfinkel <talg@vmware.com>
2011-08-18 18:20:08 -0700

    rename fail for package_cache stuff.
    Change-Id: I5b67648cb52d2c02bcb706c31dc56a404e1934bd

---
+++ package_cache/cache_test.rb
@@ -0,0 +1,51 @@
+$:.unshift(File.join(File.dirname(__FILE__)))
+require 'fileutils'
+require 'cache'
+TEST_PACKAGE = 'testpackage.tgz'
+TEST_DIR =  File.join Dir.pwd, 'test_dir'
+TEST_CACHE_ROOT = File.join TEST_DIR, 'cache_root'
+
+def create_test_file(path)
+  test_string = "I am a test file"
+  File.open(path, 'w') {|f| f.write(test_string) }
+end
+
+begin
+  Dir.mkdir(TEST_DIR) if not Dir.exists? TEST_DIR
+  Dir.chdir(TEST_DIR)
+  test_package_path = File.join Dir.pwd, TEST_PACKAGE
+  Dir.mkdir(TEST_CACHE_ROOT)
+  create_test_file(test_package_path)
+  pk = PackageCache::Cache.new(TEST_CACHE_ROOT)
+  puts "adding test package"
+  pk.add_by_rename!(test_package_path)
+  if pk.contains?(TEST_PACKAGE)
+    puts "add succeeded =)"
+  else
+    puts "add failed =("
+  end
+
+  puts "removing  test package"
+  pk.remove!(TEST_PACKAGE)
+  if not pk.contains?(TEST_PACKAGE)
+    puts "remove succeeded =)"
+  else
+    puts "remove failed =("
+  end
+
+  puts "re-adding package"
+  create_test_file(test_package_path)
+  pk.add_by_rename!(test_package_path)
+
+  puts "purging!"
+  pk.purge!
+  if not pk.contains?(TEST_PACKAGE)
+    puts "purge! succeeded =)"
+  else
+    puts "purge! failed =("
+  end
+
+ensure
+  FileUtils.rm_rf(TEST_DIR)
+end
+
