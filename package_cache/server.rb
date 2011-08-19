45d6d55ec88ea8cdb4ca23c5739795ffb4dc62a2
Tal Garfinkel <talg@vmware.com>
2011-08-18 18:08:16 -0700

    bunch of new stuff in server component.
    Change-Id: I5257dd61d80045f09466a7adec027a3d603f7e0c

--- package_cache/server.rb
+++ package_cache/server.rb
@@ -1,25 +1,69 @@
 $:.unshift(File.join(File.dirname(__FILE__)))
 require 'sinatra/base'
 require 'logger'
-require 'loader'
+require 'inbox'
+require 'support'
+require 'cache'
+require 'fileutils'
+require 'lib/vdebug'
+require 'gem_downloader'
 
-class PackageCacheApi < Sinatra::Base
-  def initialize
-    super
-    @logger = Logger.new(STDOUT)
-    @logger.info("Starting up package cache")
-    @loader = Loader.new(@logger)
-  end
-
-  put '/load/:type/:name' do |type, name|
-    if type == 'remote'
-      puts type,name
-      @loader.load_remote_gem(name)
-    elsif type == 'local'
-      @loader.load_local_gem(name)
-    else
-      raise 'invalid type'
+module PackageCache
+  class PackageCacheApi < Sinatra::Base
+    def initialize
+      super
+      @logger = Logger.new(STDOUT)
+      @logger.info("Bringing up package cache...")
+      set_directories
+      begin
+        install_directories
+        setup_components
+      rescue => e
+        @logger.error("startup up failed with exception")
+        log_exception(e)
+        exit 1
+      end
+      @logger.info("Package cache started...")
     end
-  end
 
+    def log_exception(e)
+      begin
+        @logger.error "Exception Caught (#{e.class.name}): #{e.to_s}"
+        @logger.error e
+      rescue
+        # Do nothing
+      end
+    end
+
+    def set_directories
+      @inbox_dir = 'test/inbox'
+      @downloads_dir = 'test/downloads'
+      @cache_dir = 'test/cache'
+    end
+
+    def install_directories
+      FileUtils.mkdir_p(@inbox_dir) if not Dir.exists? @inbox_dir
+      FileUtils.mkdir_p(@downloads_dir) if not Dir.exists? @downloads_dir
+      FileUtils.mkdir_p(@cache_dir)  if not Dir.exists? @cache_dir
+    end
+
+    def setup_components
+      @inbox = PackageCache::Inbox.new(@inbox_dir, :server, @logger)
+      @downloader = PackageCache::GemDownloader.new(@downloads_dir, @logger)
+      @cache = PackageCache::Cache.new(@cache_dir, @logger)
+    end
+
+    put '/load/:type/:name' do |type, name|
+      if type == 'remote'
+        puts type,name
+        #@loader.load_remote_gem(name)
+      elsif type == 'local'
+        puts type,name
+        #@loader.load_local_gem(name)
+      else
+        raise 'invalid type'
+      end
+    end
+
+  end
 end
