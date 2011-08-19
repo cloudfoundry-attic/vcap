a7b588974b1b4ae56182c8fdcb6bedf6530a0b25
Mahesh <mpatil@vmware.com>
2011-08-18 23:09:09 -0700

    Add external_uri support
    Tested my changes with cc and services configured to go against my dev23 setup
    i.e.  "appcloud23.dev.mozycloud.com"
    
    Ran BVTs.
    
    Change-Id: I4e3c97a06c4348c31d610957af6e74cf953ad5c5

--- dev_setup/cookbooks/cloud_controller/attributes/default.rb
+++ dev_setup/cookbooks/cloud_controller/attributes/default.rb
@@ -1,6 +1,6 @@
+include_recipe "deployment"
 default[:cloud_controller][:config_file] = "cloud_controller.yml"
-default[:cloud_controller][:service_api_uri] = "api.vcap.me"
-default[:cloud_controller][:domain] = "vcap.me"
+default[:cloud_controller][:service_api_uri] = "http://api.#{node[:deployment][:domain]}"
 default[:cloud_controller][:local_route] = nil
 
 # Staging
@@ -12,7 +12,6 @@ default[:cloud_controller][:staging][:platform] = "platform.yml"
 default[:cloud_controller][:staging][:rails3] = "rails3.yml"
 default[:cloud_controller][:staging][:sinatra] = "sinatra.yml"
 default[:cloud_controller][:staging][:spring] = "spring.yml"
-default[:cloud_controller][:service_api_uri] = "api.vcap.me"
 
 # Default builtin services
 default[:cloud_controller][:builtin_services] = ["redis", "mongodb", "mysql"]
