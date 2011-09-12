d4fa7cc4bf82a248f51ca767ce1a6f9a3f18857d
Albert Choi <albert.choi@gmail.com>
2011-09-12 15:13:28 -0700

    * changes:     recipes/cookbook for php     configurable vars in dea/cc/hm attributes

--- dev_setup/cookbooks/dea/attributes/default.rb
+++ dev_setup/cookbooks/dea/attributes/default.rb
@@ -1,2 +1,6 @@
 default[:dea][:config_file] = "dea.yml"
 default[:dea][:local_route] = nil
+default[:dea][:logging] = 'debug'
+default[:dea][:secure] = false
+default[:dea][:multi_tenant] = true
+default[:dea][:enforce_ulimit] = false
