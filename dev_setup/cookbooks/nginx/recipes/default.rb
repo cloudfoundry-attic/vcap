db23b4f6f6194348a40820f1871978d4abb3f351
Josh Valdez <exxonvaldeez@gmail.com>
2011-09-12 17:37:10 -0700

    Compile nginx and run for router and cc
    I rewrote the nginx recipe for the dev setup to compile from source
    and run instances for the router and cloud controller.
    
    At this point, it stores the source, configuration and supporting files
    in /var/vcap/packages and logs to /var/vcap/sys/log/nginx.
    
    The router nginx needs to be run as root to bind to port 80 though the
    cloud controller nginx should be able to be run as a normal user (it
    is currently configured to run as root as well).
    
    Please review and suggest any generalizations for the template files.
    
    * Tested on Ubuntu 10.04 VM
    * Correctly runs cloud controller and router
    * Serves sample application correctly
    
    Change-Id: Idabb46811d6a331f2239a2ba8aab3c626623f3bf

--- dev_setup/cookbooks/nginx/recipes/default.rb
+++ dev_setup/cookbooks/nginx/recipes/default.rb
@@ -6,21 +6,96 @@
 #
 #
 
-case node['platform']
-when "ubuntu"
-  package "nginx"
-  template "nginx.conf" do
-    path File.join(node[:nginx][:dir], "nginx.conf")
-    source "ubuntu-nginx.conf.erb"
-    owner "root"
-    group "root"
-    mode 0644
-  end
+%w{build-essential zlib1g zlib1g-dev libssl-dev}.each do |pkg|
+  package pkg
+end
 
-  service "nginx" do
-    supports :status => true, :restart => true, :reload => true
-    action [ :enable, :start ]
-  end
-else
-  Chef::Log.error("Installation of nginx packages not supported on this platform.")
+# Setup configure flags
+node.set[:nginx][:daemon_disable]  = true
+node.set[:nginx][:configure_flags] = [
+  "--prefix=#{node[:nginx][:prefix]}",
+  "--with-pcre=../pcre-8.12",
+  "--with-http_ssl_module",
+  "--add-module=../headers-more-v0.15rc1",
+  "--add-module=../nginx_upload_module-2.2.0"
+]
+
+configure_flags = node[:nginx][:configure_flags].join(" ")
+
+nginx_version = node[:nginx][:version]
+
+remote_file "#{Chef::Config[:file_cache_path]}/nginx-#{nginx_version}.tar.gz" do
+  source "http://sysoev.ru/nginx/nginx-#{nginx_version}.tar.gz"
+  action :create_if_missing
+end
+
+cookbook_file "#{Chef::Config[:file_cache_path]}/pcre-8.12.tar.gz" do
+  action :create_if_missing
+end
+
+cookbook_file "#{Chef::Config[:file_cache_path]}/headers-more-v0.15rc1.tgz" do
+  action :create_if_missing
+end
+
+cookbook_file "#{Chef::Config[:file_cache_path]}/nginx_upload_module-2.2.0.tar.gz" do
+  action :create_if_missing
+end
+
+cookbook_file "#{Chef::Config[:file_cache_path]}/upload_module_put_support.patch" do
+  action :create_if_missing
+end
+
+# Assumes that Ubuntu has set user and group name to be the same
+user = group = node[:deployment][:user]
+
+bash "compile_nginx_source" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    mkdir -p /var/vcap/sys/log/nginx
+    mkdir -p /var/vcap/sys/run/nginx
+
+    tar xzf pcre-8.12.tar.gz
+
+    tar xzf headers-more-v0.15rc1.tgz
+
+    tar xzf nginx_upload_module-2.2.0.tar.gz
+    cd nginx_upload_module-2.2.0
+    patch < ../upload_module_put_support.patch
+    cd ..
+
+    tar xzf nginx-#{nginx_version}.tar.gz
+    cd nginx-#{nginx_version} && ./configure #{configure_flags}
+    make && make install
+  EOH
+  creates node[:nginx][:src_binary]
+end
+
+template "router_nginx.conf" do
+  path File.join(node[:nginx][:prefix], "conf", "router_nginx.conf")
+  source "router.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+template "cloud_controller_nginx.conf" do
+  path File.join(node[:nginx][:prefix], "conf", "cloud_controller_nginx.conf")
+  source "cloud_controller.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+# nginx is used in the router and cloud controller with two different configuration files
+#
+# For now, Chef will manually start nginx as our configuration is non-standard
+# and follows how BOSH packages are installed
+
+bash "start_nginx" do
+  cwd File.join(node[:nginx][:prefix])
+  code <<-EOH
+    sudo #{node[:nginx][:prefix]}/sbin/nginx -c #{node[:nginx][:prefix]}/conf/router_nginx.conf
+    sudo #{node[:nginx][:prefix]}/sbin/nginx -c #{node[:nginx][:prefix]}/conf/cloud_controller_nginx.conf
+  EOH
+  creates node[:nginx][:started]
 end
