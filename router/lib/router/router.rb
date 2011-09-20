582e5f6abd44c1c4626e107476207744e9a6f286
Anton Panasenko <anton.panasenko@gmail.com>
2011-09-20 17:32:17 +0400

    Add rps for all apps not only top 10 in /varz
    - expose 'top' var which holds request-per-second stats for all apps
    - add expose_all_apps config parameter which determines if we expose
    rps stats for all apps
    
    Change-Id: Id66f92c66bbe500c9977f606f86346a1d287aa4b

--- router/lib/router/router.rb
+++ router/lib/router/router.rb
@@ -28,6 +28,7 @@ class Router
       @session_key = config['session_key'] || '14fbc303b76bacd1e0a3ab641c11d11400341c5d'
       @trace_key = config['trace_key'] || '22'
       @client_inactivity_timeout = config['client_inactivity_timeout'] || 60
+      @expose_all_apps = config['status']['expose_all_apps'] if config['status']
     end
 
     def setup_listeners
@@ -88,8 +89,9 @@ class Router
         end
       end
 
-      top_10 = apps.sort { |a,b| b[:rps]<=>a[:rps] } [0,9]
-      VCAP::Component.varz[:top_app_requests] = top_10
+      top = apps.sort { |a,b| b[:rps]<=>a[:rps] }
+      VCAP::Component.varz[:top_app_requests]  = top if @expose_all_apps
+      VCAP::Component.varz[:top10_app_requests]  = top[0,9]
       #log.debug "Calculated all request rates in  #{Time.now - now} secs."
     end
 
