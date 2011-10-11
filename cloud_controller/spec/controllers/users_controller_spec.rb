7ebce739891d53f677109d5345d9926dcc7d4e70
mpage <mpage@vmware.com>
2011-10-11 11:33:44 -0700

    Remove 'local_register_only' and add 'allow_registration'.
    The existing implementation of 'local_register_only' had a security hole.
    Moreover, I'm not sure that it's possible to implement the intent of 'local_register_only'
    in Rails/EM safely. We would need to grab the socket for the current request and call getpeername()
    on it.
    
    We attempt to provide similar functionality to 'local_register_only' by allowing users to disable
    registration and provide seed users to be created at startup. The seed users can then be used to
    register additional users.
    
    Test plan:
    - New unit tests pass
    - Ran CC locally with/without allow_registration set and verified the correct behaviour with vmc.
    - BVTs pass locally
    - BVTs pass on my deployment
    
    Change-Id: Ib1c96304f7a7364b913a8a09f7178f85857cd46d

--- cloud_controller/spec/controllers/users_controller_spec.rb
+++ cloud_controller/spec/controllers/users_controller_spec.rb
@@ -8,6 +8,44 @@ describe UsersController do
     request.env["HTTP_AUTHORIZATION"] = ""
   end
 
+  describe '#create' do
+    it 'should return 403 if the user is not an admin and registration is disabled' do
+      AppConfig[:allow_registration] = false
+      post_with_body :create do
+        { :email    => 'foo@bar.com',
+          :password => 'testpass',
+        }
+      end
+      response.status.should == 403
+    end
+
+    it 'should create users if the user is an admin and registration is disabled' do
+      AppConfig[:allow_registration] = false
+      User.find_by_email('foo@bar.com').should be_nil
+      @admin.admin?.should be_true
+      @admin_headers.each {|key, value| request.env[key] = value}
+      post_with_body :create do
+        { :email    => 'foo@bar.com',
+          :password => 'testpass',
+        }
+      end
+      response.status.should == 204
+      User.find_by_email('foo@bar.com').should_not be_nil
+    end
+
+    it 'should create users if the user is not an admin and registration is allowed' do
+      AppConfig[:allow_registration] = true
+      User.find_by_email('foo@bar.com').should be_nil
+      post_with_body :create do
+        { :email    => 'foo@bar.com',
+          :password => 'testpass',
+        }
+      end
+      response.status.should == 204
+      User.find_by_email('foo@bar.com').should_not be_nil
+    end
+  end
+
   describe "#list" do
     it 'should return 200 as an admin' do
       @admin.admin?.should be_true
@@ -53,5 +91,11 @@ describe UsersController do
       response.status.should == 403
       User.find_by_email(@user.email).should_not be_nil
     end
+  end
+
+  def post_with_body(*args, &blk)
+    body = yield
+    request.env['RAW_POST_DATA'] = Yajl::Encoder.encode(body)
+    post(*args)
   end
 end
