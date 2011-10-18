a9334ff488a45dd4c923ff2be8aa1c48b6a8eb94
mpage <mpage@vmware.com>
2011-10-18 11:06:25 -0700

    Refactor staging task management into a separate class.
    Add a class for representing/managing staging tasks that are active.
    Both the staging controller and the services controller will reference
    this when stagers make requests on behalf of a user during staging.
    
    Test plan:
    - Existing unit tests pass
    - Added new unit tests
    - BVTs pass
    
    Change-Id: I3a73e377915414bfa79dc28fc791b5e4f47e96a9

--- cloud_controller/app/controllers/staging_controller.rb
+++ cloud_controller/app/controllers/staging_controller.rb
@@ -6,67 +6,16 @@ class StagingController < ApplicationController
   skip_before_filter :fetch_user_from_token
   before_filter :authenticate_stager
 
-  class DropletUploadHandle
-    attr_reader :upload_id, :upload_path, :upload_uri, :app
-
-    def initialize(app)
-      @app         = app
-      @upload_id   = VCAP.secure_uuid
-      @upload_path = File.join(AppConfig[:directories][:tmpdir],
-                               "staged_upload_#{app.id}_#{@upload_id}.tgz")
-      @upload_uri  = StagingController.upload_droplet_uri(app, @upload_id)
-    end
-  end
-
-  class << self
-    def upload_droplet_uri(app, upload_id)
-      staging_uri("/staging/droplet/#{app.id}/#{upload_id}")
-    end
-
-    def download_app_uri(app)
-      staging_uri("/staging/app/#{app.id}")
-    end
-
-    def create_upload(app)
-      @uploads ||= {}
-      ret = DropletUploadHandle.new(app)
-      @uploads[ret.upload_id] = ret
-      ret
-    end
-
-    def lookup_upload(upload_id)
-      @uploads ||= {}
-      @uploads[upload_id]
-    end
-
-    def complete_upload(handle)
-      return unless @uploads
-      @uploads.delete(handle.upload_id)
-    end
-
-    private
-
-    def staging_uri(path)
-      uri = URI::HTTP.build(
-        :host     => CloudController.bind_address,
-        :port     => CloudController.external_port,
-        :userinfo => [AppConfig[:staging][:auth][:user], AppConfig[:staging][:auth][:password]],
-        :path     => path
-      )
-      uri.to_s
-    end
-  end
-
   # Handles a droplet upload from a stager
   def upload_droplet
-    upload = nil
+    task = nil
     src_path = nil
     app = App.find_by_id(params[:id])
     raise CloudError.new(CloudError::APP_NOT_FOUND) unless app
 
-    upload = self.class.lookup_upload(params[:upload_id])
-    unless upload
-      CloudController.logger.error("No upload set for upload_id=#{params[:upload_id]}")
+    task = StagingTask.find_task(params[:staging_task_id])
+    unless task
+      CloudController.logger.error("No task associated with id #{params[:staging_task_id]}")
       raise CloudError.new(CloudError::BAD_REQUEST)
     end
 
@@ -81,20 +30,18 @@ class StagingController < ApplicationController
     end
 
     begin
-      CloudController.logger.debug("Renaming staged droplet from '#{src_path}' to '#{upload.upload_path}'")
-      File.rename(src_path, upload.upload_path)
+      CloudController.logger.debug("Renaming staged droplet from '#{src_path}' to '#{task.upload_path}'")
+      File.rename(src_path, task.upload_path)
     rescue => e
       CloudController.logger.error("Failed uploading staged droplet: #{e}", :tags => [:staging])
       CloudController.logger.error(e)
-      FileUtils.rm_f(upload.upload_path)
+      FileUtils.rm_f(task.upload_path)
       raise e
     end
-    CloudController.logger.debug("Stager (#{request.remote_ip}) uploaded droplet to #{upload.upload_path}",
-                                 :tags => [:staging])
+    CloudController.logger.debug("Stager (#{request.remote_ip}) uploaded droplet to #{task.upload_path}", :tags => [:staging])
     render :nothing => true, :status => 200
   ensure
     FileUtils.rm_f(src_path) if src_path
-    self.class.complete_upload(upload) if upload
   end
 
   # Handles an app download from a stager
---
+++ cloud_controller/spec/models/staging_task_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'uri'
+
+describe StagingTask do
+  describe '.create_and_track' do
+    before :each do
+      StagingTask.untrack_all_tasks
+    end
+
+    it 'should return a new StagingTask' do
+      app  = create_stub_app(1)
+      task = StagingTask.create_and_track(app)
+      task.class.should == StagingTask
+    end
+
+    it 'should add tasks to the internal tracking hash so they can be looked up later' do
+      app = create_stub_app(1)
+      task = StagingTask.create_and_track(app)
+      StagingTask.find_task(task.task_id).should == task
+    end
+  end
+
+  describe '.untrack' do
+    before :each do
+      StagingTask.untrack_all_tasks
+    end
+
+    it 'should remove tasks from the internal tracking hash' do
+      app = create_stub_app(1)
+      task = StagingTask.create_and_track(app)
+      StagingTask.find_task(task.task_id).should == task
+      StagingTask.untrack(task)
+      StagingTask.find_task(task.task_id).should be_nil
+    end
+  end
+
+  describe '#staging_uri' do
+    it 'should correctly set the host and port' do
+      CloudController.stubs(:bind_address).returns('127.0.0.1')
+      CloudController.stubs(:external_port).returns(12345)
+      task = StagingTask.new(create_stub_app(1))
+      uri = task.send(:staging_uri, '/test/path')
+      parsed_uri = URI.parse(uri)
+      parsed_uri.host.should == '127.0.0.1'
+      parsed_uri.port.should == 12345
+    end
+  end
+
+  def create_stub_app(id)
+    app = stub("app_#{id}")
+    app.stubs(:id).returns(id)
+    app
+  end
+end
