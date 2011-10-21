0253d73ef93ce462b0e37cd2829bd7b929bf14c9
viovanov <vlad.iovanov@uhurusoftware.com>
2011-10-21 11:19:21 +0300

    Added .net runtime to CloudFoundry

--- cloud_controller/app/models/app.rb
+++ cloud_controller/app/models/app.rb
@@ -23,8 +23,8 @@ class App < ActiveRecord::Base
 
   AppStates = %w[STOPPED STARTED]
   PackageStates = %w[PENDING STAGED FAILED]
-  Runtimes = %w[ruby18 ruby19 java node php erlangR14B02 python26]
-  Frameworks = %w[sinatra rails3 java_web spring grails node php otp_rebar lift wsgi django unknown]
+  Runtimes = %w[ruby18 ruby19 java node php erlangR14B02 python26 iis]
+  Frameworks = %w[sinatra rails3 java_web spring grails node php otp_rebar lift wsgi django .net unknown]
 
   validates_presence_of :name, :framework, :runtime
 
