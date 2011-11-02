1864485a9f6d23f75624318a276732ba905cd69f
Bob Nugmanov <bnugmanov@vmware.com>
2011-11-02 12:33:14 -0700

    [hm2][bulk_api] full-thread hm2 ver 0.0.2 [preview]
    full-thread functionality
    component structure finalized
    directory structure finalized
    bulk api updated
    
    suggested reviewing order: README.md, harmonizer.rb, app_state_provider.rb, ...
    
    Change-Id: I11521bf421ffde8530fa1ee27de7e22eaa35d75d

---
+++ hm-2/lib/hm-2.rb
@@ -0,0 +1,116 @@
+#hm-2 -- HealthManager 2.0. (c) 2011-2012 VMware, Inc.
+$:.unshift(File.dirname(__FILE__))
+
+require 'yajl'
+require 'nats/client'
+
+require 'vcap/common'
+require 'vcap/component'
+require 'vcap/logging'
+require 'vcap/priority_queue'
+
+require 'hm-2/constants'
+require 'hm-2/app_state_provider'
+require 'hm-2/nats_based_known_state_provider'
+require 'hm-2/bulk_based_expected_state_provider'
+require 'hm-2/scheduler'
+require 'hm-2/nudger'
+require 'hm-2/harmonizer'
+
+module HealthManager2
+
+  class Manager
+    #primarily for testing
+    attr_reader :scheduler
+    attr_reader :known_state_provider
+    attr_reader :expected_state_provider
+
+    def initialize(config={})
+      @config = config
+      @logger = get_logger
+
+      @logger.info("HM-2: HealthManager2 initializing")
+
+      @scheduler = Scheduler.new(config)
+      register_hm_component(:scheduler, @scheduler, @config)
+
+      @known_state_provider = AppStateProvider.get_known_state_provider(@config)
+      register_hm_component(:known_state_provider, @known_state_provider, @config)
+
+      @expected_state_provider = AppStateProvider.get_expected_state_provider(@config)
+      register_hm_component(:expected_state_provider, @expected_state_provider, @config)
+
+      @nudger = Nudger.new(@config)
+      register_hm_component(:nudger, @nudger, @config)
+      @harmonizer = Harmonizer.new(@config)
+    end
+
+    def start
+      @logger.info("starting...")
+
+      NATS.start :uri => ENV['NATS_URI'] || 'nats://nats:nats@192.168.24.128:4222' do #TODO: nats_uri through config
+        @harmonizer.prepare
+        @expected_state_provider.start
+        @known_state_provider.start
+        @scheduler.start #blocking call
+      end
+    end
+
+    def shutdown
+      @logger.info("shutting down...")
+      NATS.stop { EM.stop }
+      @logger.info("...good bye.")
+    end
+
+    #The setter for @now this is defined in the spec helper
+    #and is used exclusively for testing
+    def self.now
+      @now || Time.now.to_i
+    end
+  end
+
+  def now
+    Manager.now
+  end
+  def get_interval_from_config_or_constant(name, config)
+    intervals = config[:intervals] || config['intervals'] || {}
+    get_param_from_config_or_constant(name,intervals)
+  end
+
+  def get_param_from_config_or_constant(name, config)
+    value = config[name] || config[name.to_sym] || config[name.to_s]
+    unless value
+      const_name = name.to_s.upcase
+      if HealthManager2.const_defined?( const_name )
+        value = HealthManager2.const_get( const_name )
+      end
+    end
+    raise ArgumentError, "undefined parameter #{name}" unless value
+    value
+  end
+
+  def register_hm_component(name, component, config)
+    hm_registry(config)[name] = component
+  end
+
+  def find_hm_component(name, config)
+    unless component = hm_registry(config)[name]
+      raise ArgumentError, "component #{name} can't be found in the registry #{config}"
+    end
+    component
+  end
+
+  def get_logger(name='hm-2')
+    VCAP::Logging.logger(name)
+  end
+
+  def hm_registry(config)
+    config[:health_manager_component_registry] ||= {}
+  end
+  def encode_json(obj={})
+    Yajl::Encoder.encode(obj)
+  end
+  def parse_json(string='{}')
+    Yajl::Parser.parse(string)
+  end
+end
---
+++ hm-2/lib/hm-2/bulk_based_expected_state_provider.rb
@@ -0,0 +1,96 @@
+require 'em-http'
+
+module HealthManager2
+  #this implementation will use the REST(ish) BulkAPI to
+  #interrogate the CloudController on the expected state of the apps
+  #the API should allow for non-blocking operation
+  class BulkBasedExpectedStateProvider < ExpectedStateProvider
+
+    def each_droplet(&block)
+      process_next_batch({},&block)
+    end
+
+    def set_expected_state(known, expected)
+
+      @logger.debug { "bulk: #set_expected_state: known: #{known.inspect} expected: #{expected.inspect}" }
+
+      known.num_instances = expected['instances']
+      known.state = expected['state']
+      known.live_version = "#{expected['staged_package_hash']}-#{expected['run_count']}"
+      known.framework = expected['framework']
+      known.runtime = expected['runtime']
+      known.last_updated = expected['update_at']
+    end
+
+    private
+    def process_next_batch(bulk_token,&block)
+      with_credentials do |user, password|
+        options = {
+          :head => { 'authorization' => [user,password] },
+          :query => {
+            'batch_size' => batch_size,
+            'bulk_token' => bulk_token.to_json
+          },
+        }
+        http = EM::HttpRequest.new(app_url).get(options)
+        http.callback {
+
+          if http.response_header.status != 200
+            @logger.error("bulk request problem. Response: #{http.response_header} #{http.response}")
+            next
+          end
+
+          response = parse_json(http.response)
+          bulk_token = response['bulk_token']
+          batch = response['results']
+          next if batch.nil? || batch.empty?
+
+          @logger.debug {"bulk api batch of size #{batch.size} received"}
+
+          batch.each do |app_id, droplet|
+            block.call(app_id, droplet)
+          end
+          process_next_batch(bulk_token, &block)
+        }
+        http.errback {
+          @logger.error("problem talking to bulk API at #{app_url}")
+          @user = @password = nil #ensure re-acquisition of credentials
+        }
+      end
+    end
+
+    def host
+      (@config['bulk'] && @config['bulk']['host']) || "api.vcap.me"
+    end
+
+    def batch_size
+      (@config['bulk'] && @config['bulk']['batch_size']) || "50"
+    end
+
+    def app_url
+      url = "#{host}/bulk/apps"
+      url = "http://"+url unless url.start_with?("http://")
+      url
+    end
+
+    def with_credentials
+      if @user && @password
+        yield @user, @password
+      else
+        @logger.info("requesting bulk API credentials over NATS...")
+        sid = NATS.request('cloudcontroller.bulk.credentials') do |response|
+          @logger.info("...bulk API credentials received: #{response}")
+          auth =  parse_json(response)
+          @user = auth[:user] || auth['user']
+          @password = auth[:password] || auth['password']
+          yield @user, @password
+        end
+
+        NATS.timeout(sid,
+                     get_param_from_config_or_constant(:nats_request_timeout,@config)) do
+          @logger.error("NATS timeout getting bulk api credentials. Jussayin'... Request ignored.")
+        end
+      end
+    end
+  end
+end
