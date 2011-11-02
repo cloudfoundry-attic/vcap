2695cb8229fc1b97feb22274765d5900f2565003
Bob Nugmanov <bnugmanov@vmware.com>
2011-11-02 12:33:14 -0700

    [hm2][bulk_api] full-thread hm2 ver 0.0.2 [preview]
    full-thread functionality
    component structure finalized
    directory structure finalized
    bulk api updated
    
    suggested reviewing order: README.md, harmonizer.rb, app_state_provider.rb, ...
    
    Change-Id: I11521bf421ffde8530fa1ee27de7e22eaa35d75d

---
+++ hm-2/lib/hm-2/constants.rb
@@ -0,0 +1,35 @@
+require 'set'
+
+module HealthManager2
+
+
+  #restart priorities
+  LOW_PRIORITY = 1
+  NORMAL_PRIORITY = 1000
+  HIGH_PRIORITY = 1_000_000
+
+
+  #intervals
+  EXPECTED_STATE_UPDATE = 10
+  ANALYSIS_DELAY = 5
+  DROPLET_ANALYSIS = 10
+  REQUEST_QUEUE = 1
+  DEA_TIMEOUT_INTERVAL = 15
+  NATS_REQUEST_TIMEOUT = 5
+  RUN_LOOP_INTERVAL = 2
+
+
+  #app states
+  DOWN              = 'DOWN'
+  STARTED           = 'STARTED'
+  STOPPED           = 'STOPPED'
+  CRASHED           = 'CRASHED'
+  STARTING          = 'STARTING'
+  RUNNING           = 'RUNNING'
+  FLAPPING          = 'FLAPPING'
+  DEA_SHUTDOWN      = 'DEA_SHUTDOWN'
+  DEA_EVACUATION    = 'DEA_EVACUATION'
+  APP_STABLE_STATES = Set.new([STARTED, STOPPED])
+  RUNNING_STATES    = Set.new([STARTING, RUNNING])
+  RESTART_REASONS   = Set.new([CRASHED, DEA_SHUTDOWN, DEA_EVACUATION])
+end
