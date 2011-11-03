0332430cda992e58b9b06acd853f13325f8a49d4
SonicWang <wangs@vmware.com>
2011-11-03 16:55:34 -0700

    add chef scripts for blob service; hack chef launch script to facilitate installing blob from a local tarball (temporary solution when vblob is open-sourced this will be fixed)
    Change-Id: Ifbd46bf06fc5b65748601c9344fe83da9bae0149

--- dev_setup/lib/chefsolo_launch.rb
+++ dev_setup/lib/chefsolo_launch.rb
@@ -40,6 +40,8 @@ spec["deployment"]["group"] ||= `id -g`.strip
 spec["cloudfoundry"] ||= {}
 spec["cloudfoundry"]["home"] ||= cloudfoundry_home
 spec["cloudfoundry"]["home"] = File.expand_path(spec["cloudfoundry"]["home"])
+# hack for blob, need to copy local binary package for deployment, must know script path
+spec["blob_dir"] = File.expand_path(File.join("..", "cookbooks", "blob", "packages"), script_dir)
 
 if cloudfoundry_home != Deployment.get_cloudfoundry_home && cloudfoundry_home != spec["cloudfoundry"]["home"]
   puts "Conflicting values for cloudfoundry home directory, command line argument says #{cloudfoundry_home} but config file says #{spec["cloudfoundry"]["home"]}"
