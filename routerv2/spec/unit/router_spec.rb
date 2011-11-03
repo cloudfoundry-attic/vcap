9785cd2145b90dd66098e89ce3f1196fbad9bbaa
junxiaotime <jxiao@vmware.com>
2011-11-03 10:23:31 -0700

    Minor update unix socket in routerv2 unit test
    Make it to be consistent with nginx config deployed by vcap_dev_setup
    
    Test done:
      - unit test
    
    Router v2 dev cloud deployment
    
    - Address most of the inline comments from Matt/Pat
    
    - Support deploying router v2 in dev cloud.
      - In devbox.yml, you can explicitly specify routerv2 install job,
        then vcap_dev_setup will help to setup right nginx image and config.
      - router and routerv2 must be exclusive in devbox.yml.
      - "install all" in devbox.yml only set up router.
    
    For Matt's general comments on moving to a simple sinatra and using
    some other mechanisms to define msgs between ULS and nginx, I suggest
    defering it as future enhancement since they are not functionally critical.
    
    For Pat's comment on moving lua module from dev_setup, those pieces
    under dev_setup are only for dev cloud setup, for bosh deployment,
    there will be dedicated bosh packages/jobs definition, and I'll send out
    a separate review against release repo.
    
    For Pat's comment on cosolidate routerv2 into router directory, it's
    complicated since client_connection.rb and functional test are mostly
    different, and keep them separate can help to easily switch between them.
    
    Test done:
    - dev cloud w/ routerv2 deployed by vcap_dev_setup
    - unit test
    - local BVT
    
    First pass for routerv2 implementation
    
    routerv2 decouples control and data path to some extent, it's a logic router
    including both nginx and uls (upstream location server) parts.
    
    uls (aka control plane) is based on legacy router:
    - all communications with nats are kept the same as before
    - client facing interface (actually connected to nginx) is simplified to
      handle the location query/stats update from nginx only.
    - the original app facing interface is totally removed.
    
    nginx (aka data plane)
    - package lua in nginx so most functionalities can be implemented with it.
    - for each request, nginx will generate a subrequest (to fit in nginx
      single thread event model) which carries location query/stats info to uls,
      the response of the subrequest will return backend addr and also an
      opaque tags (only known by uls), and the corresponding main request will
      also be awaked and routed to the backend server, when a response from backend
      is returned back, the counter associated with the particular opaque tags
      will be incremented accoring to the response code. The next incoming
      request will always help to carry not-synced stats to uls along with
      its location query.
    - sticky session and trace header are also implemented here,
      also their key related config are moved to nginx part.
    
    functional testing
    - this pass rewrites most of function tests to test both uls + nginx together,
      and also add a few more tests simulating e2e scenario.
    
    This pass doesn't change deployment behavior, and more focus on core logic.
    
    The functional tests can be done as following:
    - change default[:nginx][:router_version] to "v2"
      in vcap/dev_setup/cookbooks/nginx/attributes/default.rb,
      vcap_dev_setup will help to build correct nginx image and run it.
    - run `bundle exec rake spec` under vcap/routerv2/spec to run all tests.
    
    Change-Id: If1f74ec6c590f2f4cf8a7efcdefafba513c29e3d

---
+++ routerv2/spec/unit/router_spec.rb
@@ -0,0 +1,137 @@
+# Copyright (c) 2009-2011 VMware, Inc.
+require File.dirname(__FILE__) + '/../spec_helper'
+
+require 'router/router'
+require 'logger'
+
+require 'vcap/logging'
+require 'vcap/rolling_metric'
+
+module VCAP
+  class Component
+    class << self
+      attr_reader :varz
+      def setup
+        @varz = {}
+      end
+    end
+  end
+end
+
+describe Router do
+  describe 'Router.config' do
+    before :each do
+      clear_router
+    end
+  end
+
+  describe 'Router.register_droplet' do
+    before :each do
+      clear_router
+    end
+
+    it 'should register a droplet' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      VCAP::Component.varz[:droplets].should == 1
+      VCAP::Component.varz[:urls].should == 1
+    end
+
+    it 'should allow proper lookup' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      droplets = Router.lookup_droplet('foo.vcap.me')
+      droplets.should be_instance_of Array
+      droplets.should have(1).items
+
+      droplet = droplets.first
+      droplet.should have_key :url
+      droplet.should have_key :timestamp
+      droplet.should have_key :requests
+      droplet.should have_key :host
+      droplet.should have_key :port
+      droplet.should have_key :clients
+      droplet[:connections].should == []
+      droplet[:requests].should == 0
+      droplet[:url].should == 'foo.vcap.me'
+      droplet[:host].should == '10.0.1.22'
+      droplet[:port].should == 2222
+      droplet[:clients].should == {}
+    end
+
+    it 'should count droplets independent of URL' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {})
+      VCAP::Component.varz[:droplets].should == 2
+      VCAP::Component.varz[:urls].should == 1
+    end
+
+    it 'should return multiple droplets for a url when they exist' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {})
+      droplets = Router.lookup_droplet('foo.vcap.me')
+      droplets.should be_instance_of Array
+      droplets.should have(2).items
+    end
+
+    it 'should ignore duplicates' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {})
+      VCAP::Component.varz[:droplets].should == 2
+      VCAP::Component.varz[:urls].should == 1
+    end
+
+    it 'should record tags' do
+      VCAP::Component.varz[:tags]["component"].should be_nil
+      Router.register_droplet('foobar.vcap.me', '10.0.1.22', 2225, {"component" => "test"})
+      VCAP::Component.varz[:tags]["component"]["test"].should_not be_nil
+      droplets = Router.lookup_droplet('foobar.vcap.me')
+      droplets.first[:tags].should == {"component" => "test"}
+    end
+
+  end
+
+  describe 'Router.unregister_droplet' do
+    before :each do
+      clear_router
+    end
+
+    it 'should unregister a droplet' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {})
+      Router.unregister_droplet('foo.vcap.me', '10.0.1.22', 2224)
+      VCAP::Component.varz[:droplets].should == 1
+      VCAP::Component.varz[:urls].should == 1
+    end
+
+    it 'should unregister a droplet that had tags' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {})
+      Router.register_droplet('foobar.vcap.me', '10.0.1.22', 2225, {"component" => "test"})
+      Router.unregister_droplet('foobar.vcap.me', '10.0.1.22', 2225)
+      VCAP::Component.varz[:droplets].should == 1
+      VCAP::Component.varz[:urls].should == 1
+    end
+
+    it 'should not return unregistered items' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      Router.unregister_droplet('foo.vcap.me', '10.0.1.22', 2222)
+      droplets = Router.lookup_droplet('foo.vcap.me')
+      droplets.should be_nil
+    end
+
+    it 'should properly account for urls and droplets' do
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {})
+      Router.unregister_droplet('foo.vcap.me', '10.0.1.22', 2222)
+      VCAP::Component.varz[:droplets].should == 0
+      VCAP::Component.varz[:urls].should == 0
+    end
+  end
+
+  def clear_router
+    Router.config({})
+    Router.instance_variable_set(:@log, double(:black_hole).as_null_object)
+    VCAP::Component.setup
+    VCAP::Component.varz[:urls] = 0
+    VCAP::Component.varz[:droplets] = 0
+    VCAP::Component.varz[:tags] = {}
+  end
+end
