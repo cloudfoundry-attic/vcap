4ec1517f5a517ecbf57efefa7cc5eab3dcdf1f82
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-11-14 14:48:57 -0800

    Add warden: manager for ephemeral Linux containers
    Change-Id: I3913e5be6392ffec183e9eb58d933988c15deafb

---
+++ warden/spec/client_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+require "warden/client"
+
+describe Warden::Client do
+
+  include_context :warden
+
+  let(:client) {
+    Warden::Client.new(unix_domain_path)
+  }
+
+  it "triggers the connected event" do
+    em do
+      client.on(:connected) { done }
+    end
+  end
+
+  it "calls deferred callback for successful commands" do
+    em do
+      deferrable = client.call("ping")
+      deferrable.callback { |reply|
+        reply.should == "pong"
+        done
+      }
+      deferrable.errback { |reply|
+        fail
+      }
+    end
+  end
+
+  it "calls deferred errback for failed commands" do
+    em do
+      deferrable = client.call("foo")
+      deferrable.callback { |reply|
+        fail
+      }
+      deferrable.errback { |reply|
+        reply.message.should match(/unknown command/)
+        done
+      }
+    end
+  end
+end
---
+++ warden/warden.gemspec
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+require "warden/version"
+
+Gem::Specification.new do |s|
+  s.name        = "warden"
+  s.version     = Warden::VERSION
+  s.authors     = ["Pieter Noordhuis"]
+  s.email       = ["pcnoordhuis@gmail.com"]
+  s.homepage    = ""
+  s.summary     = %q{TODO: Write a gem summary}
+  s.description = %q{TODO: Write a gem description}
+
+  s.rubyforge_project = "warden"
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  s.add_runtime_dependency "eventmachine"
+  s.add_runtime_dependency "hiredis", "~> 0.4.0"
+end
