7eababdc02056a5f0f2584f1d19db41e974da64e
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-12-05 22:50:23 -0800

    warden: Make container configuration as minimal as possible
    Change-Id: I1e4c8104bdd05bccfe9242ec9946787b31b1fbf8

--- warden/root/lxc/010-misc/setup.rb
+++ warden/root/lxc/010-misc/setup.rb
@@ -6,26 +6,27 @@ require "../.lib/global"
 
 Dir.chdir("union")
 
-write "etc/fstab", <<-EOS
-tmpfs /dev/shm tmpfs defaults 0 0
+write "lib/init/fstab", <<-EOS
+# nothing
 EOS
 
-# Disable unneeded mount points
-file = "lib/init/fstab"
-write file, IO.readlines(file).
-  find_all { |line|
-    line !~ %r!\s/(proc|sys|spu|dev)\s!i
-  }.join
-
 # Disable unneeded services
-sh "rm -f etc/init/tty*"
 sh "rm -f etc/init/ureadahead*"
 sh "rm -f etc/init/plymouth*"
 sh "rm -f etc/init/hwclock*"
 sh "rm -f etc/init/hostname*"
-
-# Removing these udev upstart files causes tty1 not to work
-sh "rm -f etc/init/udev{monitor,trigger,-finish}.conf"
+sh "rm -f etc/init/*udev*"
+sh "rm -f etc/init/module-*"
+sh "rm -f etc/init/mountall-*"
+sh "rm -f etc/init/mounted-*"
+sh "rm -f etc/init/dmesg*"
+sh "rm -f etc/init/network-*"
+sh "rm -f etc/init/procps*"
+sh "rm -f etc/init/rcS*"
+sh "rm -f etc/init/rsyslog*"
 
 # Don't run ntpdate when container network comes up
 sh "rm -f etc/network/if-up.d/ntpdate"
+
+# Don't run cpu frequency scaling
+sh "rm -f etc/rc*.d/S*ondemand"
