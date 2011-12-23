08f79d7d4be11a9b336796a33e98a1098188c796
Jesse Zhang <jessezhang@vmware.com>
2011-12-23 01:30:28 -0800

    correct erlang installation path
    Change-Id: I32672737bc23b1266e08cb9d431f53b9ac929b90

--- dev_setup/cookbooks/erlang/attributes/default.rb
+++ dev_setup/cookbooks/erlang/attributes/default.rb
@@ -1,4 +1,4 @@
 include_attribute "deployment"
 default[:erlang][:version] = "R14B02"
 default[:erlang][:source]  = "http://erlang.org/download/otp_src_#{erlang[:version]}.tar.gz"
-default[:erlang][:path]    = File.join(node[:deployment][:home], "deploy", "erlang")
+default[:erlang][:path]    = "/var/vcap/runtimes/erlang-#{erlang[:version]}"
