ba145ea23a0c7d4ff50583880f60c13aef331333
Satoru Chinen <satoru.chinen@gmail.com>
2011-12-26 09:46:55 +0900

    Fix unsolved problems for install Ruby 1.8.

--- dev_setup/cookbooks/ruby/recipes/ruby18.rb
+++ dev_setup/cookbooks/ruby/recipes/ruby18.rb
@@ -5,7 +5,23 @@ rubygems_version = node[:rubygems][:version]
 bundler_version = node[:rubygems][:bundler][:version]
 rake_version = node[:rubygems][:rake][:version]
 
-include_recipe "ruby::default"
+%w[ build-essential libssl-dev zlib1g-dev libreadline5-dev libxml2-dev libpq-dev].each do |pkg|
+  package pkg
+end
+
+remote_file File.join("", "tmp", "ruby-#{ruby_version}.tar.gz") do
+  owner node[:deployment][:user]
+  source ruby_source
+  not_if { ::File.exists?(File.join("", "tmp", "ruby-#{ruby_version}.tar.gz")) }
+end
+
+directory ruby_path do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
+end
 
 bash "Install Ruby 1.8" do
   cwd File.join("", "tmp")
@@ -22,6 +38,12 @@ bash "Install Ruby 1.8" do
   end
 end
 
+remote_file File.join("", "tmp", "rubygems-#{rubygems_version}.tgz") do
+  owner node[:deployment][:user]
+  source "http://production.cf.rubygems.org/rubygems/rubygems-#{rubygems_version}.tgz"
+  not_if { ::File.exists?(File.join("", "tmp", "rubygems-#{rubygems_version}.tgz")) }
+end
+
 bash "Install RubyGems for Ruby 1.8" do
   cwd File.join("", "tmp")
   user node[:deployment][:user]
@@ -33,5 +55,24 @@ bash "Install RubyGems for Ruby 1.8" do
   not_if do
     ::File.exists?(File.join(ruby_path, "bin", "gem")) &&
         system("#{File.join(ruby_path, "bin", "gem")} -v | grep -q '#{rubygems_version}$'")
+  end
+end
+
+gem_package "bundler" do
+  version bundler_version
+  gem_binary File.join(ruby_path, "bin", "gem")
+end
+
+gem_package "rake" do
+  version rake_version
+  gem_binary File.join(ruby_path, "bin", "gem")
+end
+
+# The default chef installed with Ubuntu 10.04 does not support the "retries" option
+# for gem_package. It may be a good idea to add/use that option once the ubuntu
+# chef package gets updated.
+%w[ rack eventmachine thin sinatra mysql pg vmc ].each do |gem|
+  gem_package gem do
+    gem_binary File.join(ruby_path, "bin", "gem")
   end
 end
