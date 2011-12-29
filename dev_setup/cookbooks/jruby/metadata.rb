ca53ae9ab53f38a577d9dcb7f487973deafee33d
Satoru Chinen <satoru.chinen@gmail.com>
2011-12-29 15:47:15 +0900

    Fix maintainer info.

--- dev_setup/cookbooks/jruby/metadata.rb
+++ dev_setup/cookbooks/jruby/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "VMware"
-maintainer_email "support@vmware.com"
+maintainer       "Satoru Chinen"
+maintainer_email "satoru.chinen@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures JRuby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
