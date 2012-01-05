1aed4365563eac493d209d30ce33546ac74077e3
Dave Syer <dsyer@vmware.com>
2012-01-05 16:14:23 +0000

    CFID-69: auth deployment for uaa and acm
    Change-Id: I3a44ddb269c3e866798ec42eaf4c7082d52fea9c

---
+++ dev_setup/cookbooks/uaa/attributes/default.rb
@@ -0,0 +1,8 @@
+include_attributes "uaadb"
+default[:uaadb][:host] = "localhost"
+default[:uaa][:varz][:password] = "varzclientsecret"
+default[:uaa][:scim][:password] = "scimsecret"
+default[:uaa][:my][:password] = "myclientsecret"
+default[:uaa][:app][:password] = "appclientsecret"
+
+
