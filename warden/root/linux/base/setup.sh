02aadae31913e45a5fc6b528b0a9fd96cdaa12aa
mpage <mpage@vmware.com>
2012-01-18 13:53:28 -0800

    [warden] Allow base chroot specification during setup
    This adds the ability to specify a specific directory to install the chroot in,
    as well as flags to disable debootstrap and apt setup.
    
    Test plan:
    - rake setup && rake spec
    
    Change-Id: I8064aa2b757911f3241da26337a40ff1fb1d254b

--- warden/root/linux/base/setup.sh
+++ warden/root/linux/base/setup.sh
@@ -3,8 +3,6 @@
 set -o nounset
 set -o errexit
 
-cd $(dirname $(readlink -f ${0}))
-
 debootstrap_bin=$(which debootstrap)
 chroot_bin=$(which chroot)
 packages="ubuntu-minimal"
@@ -37,16 +35,28 @@ function chroot() {
   ${chroot_bin} ${target} env -i /bin/bash
 }
 
-# Do debootstrap when specified
-if [ "$#" -gt 0 ] && [ "$1" == "debootstrap" ]; then
+if [ ${EUID} -ne 0 ]; then
+  echo "Sorry, you need to be root."
+  exit 1
+fi
+
+if [ "${#}" -ne 1 ]; then
+  echo "Usage: setup.sh [base_dir]"
+  exit 1
+fi
+
+if [ ! -e ${1} ]; then
+  echo "Looks like ${1} doesn't exist"
+  exit 1
+fi
+
+cd ${1}
+
+if [ -z "${SKIP_DEBOOTSTRAP+1}" ]; then
   debootstrap
 fi
 
-# Do debootstrap when target directory does not yet exist
-if [ ! -d ${target} ]; then
-  debootstrap
-fi
-
+if [ -z "${SKIP_APT+1}" ]; then
 write "etc/apt/sources.list" <<-EOS
 deb ${mirror} lucid main universe
 deb ${mirror} lucid-updates main universe
@@ -55,7 +65,7 @@ EOS
 # Disable interactive dpkg
 chroot <<-EOS
 echo debconf debconf/frontend select noninteractive |
-  debconf-set-selections
+ debconf-set-selections
 EOS
 
 # Install packages
@@ -70,6 +80,7 @@ rm -f /var/cache/apt/archives/*.deb
 rm -f /var/cache/apt/*cache.bin
 rm -f /var/lib/apt/lists/*_Packages
 EOS
+fi
 
 write "lib/init/fstab" <<-EOS
 # nothing
