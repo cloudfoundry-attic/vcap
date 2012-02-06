311d635f9cef9b480bbe4f5c9eb51b3d5eba3bc2
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2012-02-06 11:21:08 -0800

    - Added sample .htaccess file to PHP fixture
    - Added test to PHP staging spec to confirm that dot files are not being lost

--- staging/spec/unit/php_spec.rb
+++ staging/spec/unit/php_spec.rb
@@ -44,4 +44,12 @@ wait $STARTED
       EXPECTED
     end
   end
+
+  it "is packaged without missing dot files like .htaccess" do
+    stage :php do |staged_dir|
+      app_dir = File.join(staged_dir, 'app')
+      app_files = Dir.entries(app_dir)
+      app_files.should include('.htaccess')
+    end
+  end
 end
