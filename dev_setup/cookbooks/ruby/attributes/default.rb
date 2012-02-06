19e2eb9649d2b60235b102147878d7a1c98278bb
Andrea Campi <andrea.campi@zephirworks.com>
2012-02-06 21:56:08 +0100

    Bump ruby versions to the current stable ones.

--- dev_setup/cookbooks/ruby/attributes/default.rb
+++ dev_setup/cookbooks/ruby/attributes/default.rb
@@ -1,5 +1,5 @@
 include_attribute "deployment"
-default[:ruby][:version] = "1.9.2-p180"
+default[:ruby][:version] = "1.9.2-p290"
 default[:ruby][:source]  = "http://ftp.ruby-lang.org//pub/ruby/1.9/ruby-#{ruby[:version]}.tar.gz"
 default[:ruby][:path]    = File.join(node[:deployment][:home], "deploy", "rubies", "ruby-#{ruby[:version]}")
 
