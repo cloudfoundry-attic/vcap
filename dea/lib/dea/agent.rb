cloudfoundry-attic/vcap

commit 2b56b7f1e4aa2a382f8b7278ff3c0332aee7f958

homogenize start/stop generator script
Conflicts:
	cloud_controller/app/models/app.rb
	cloud_controller/staging/common.rb
	dea/config/dea.yml
	docs/php.md

Conflicts:
	cloud_controller/Gemfile.lock
	cloud_controller/vendor/cache/vcap_staging-0.1.2.gem
	staging/lib/vcap/staging/plugin/common.rb
	staging/lib/vcap/staging/version.rb
	staging/spec/unit/spring_spec.rb

Change-Id: Ia8a1d138bb5961f501b195f9bb6dcfe26d8ba93d

dea/lib/dea/agent.rb
@@ -1186,12 +1186,13 @@ module DEA
         instance[:state_timestamp] = Time.now.to_i
         stop_cmd = File.join(instance[:dir], 'stop')
         stop_cmd = "su -c #{stop_cmd} #{username}" if @secure
-        stop_cmd = "#{stop_cmd} 2> /dev/null"
+        stop_cmd = "#{stop_cmd} #{instance[:pid]} 2> /dev/null"
 
         unless (RUBY_PLATFORM =~ /darwin/ and @secure)
           @logger.debug("Executing stop script: '#{stop_cmd}'")
           # We can't make 'stop_cmd' into EM.system because of a race with
           # 'cleanup_droplet'
+          @logger.debug("Stopping instance PID:#{instance[:pid]}")
           Bundler.with_clean_env { system(stop_cmd) }
         end
       end

cloud_controller/Gemfile
@@ -10,7 +10,7 @@ gem 'logging', '>= 1.5.0'
 # VCAP common components
 gem 'vcap_common', :require => ['vcap/common', 'vcap/component'], :path => '../common'
 gem 'vcap_logging', :require => ['vcap/logging']
-gem 'vcap_staging', '= 0.1.24'
+gem 'vcap_staging', '= 0.1.25'
 
 # For queuing staging tasks
 gem 'em-hiredis'

stager/Gemfile
@@ -8,7 +8,7 @@ gem 'yajl-ruby', '>= 0.7.9'
 
 gem 'vcap_common'
 gem 'vcap_logging', '>= 0.1.3'
-gem 'vcap_staging', '>= 0.1.24'
+gem 'vcap_staging', '>= 0.1.25'
 
 group :test do
   gem 'rspec'

staging/lib/vcap/staging/plugin/common.rb
@@ -343,6 +343,15 @@ class StagingPlugin
     app_server['executable']
   end
 
+  def stop_command
+    cmds = []
+    cmds << 'APP_PID=$1'
+    cmds << 'APP_PPID=`ps -o ppid= -p $APP_PID`'
+    cmds << 'kill -9 $APP_PID'
+    cmds << 'kill -9 $APP_PPID'
+    cmds.join("\n")
+  end
+
   def local_runtime
     '%VCAP_LOCAL_RUNTIME%'
   end
@@ -429,16 +438,6 @@ class StagingPlugin
     "cd app"
   end
 
-  def generate_stop_script
-    <<-SCRIPT
-echo "$STARTED" >> ../run.pid
-echo "#!/bin/bash" >> ../stop
-echo "kill -9 $STARTED" >> ../stop
-echo "kill -9 $PPID" >> ../stop
-chmod 755 ../stop
-    SCRIPT
-  end
-
   def get_launched_process_pid
     "STARTED=$!"
   end
@@ -456,12 +455,21 @@ chmod 755 ../stop
 <%= change_directory_for_start %>
 <%= start_command %> > ../logs/stdout.log 2> ../logs/stderr.log &
 <%= get_launched_process_pid %>
-<%= generate_stop_script %>
+echo "$STARTED" >> ../run.pid
 <%= wait_for_launched_process %>
     SCRIPT
     # TODO - ERB is pretty irritating when it comes to blank lines, such as when 'after_env_before_script' is nil.
     # There is probably a better way that doesn't involve making the above Heredoc horrible.
     ERB.new(template).result(binding).lines.reject {|l| l =~ /^\s*$/}.join
+  end
+
+  def generate_stop_script(env_vars = {})
+    template = <<-SCRIPT
+#!/bin/bash
+<%= environment_statements_for(env_vars) %>
+<%= stop_command %>
+    SCRIPT
+    ERB.new(template).result(binding)
   end
 
   # Generates newline-separated exports for the specified environment variables.
@@ -481,6 +489,14 @@ chmod 755 ../stop
   def create_app_directories
     FileUtils.mkdir_p File.join(destination_directory, 'app')
     FileUtils.mkdir_p File.join(destination_directory, 'logs')
+  end
+
+  def create_stop_script()
+    path = File.join(destination_directory, 'stop')
+    File.open(path, 'wb') do |f|
+      f.puts stop_script
+    end
+    FileUtils.chmod(0500, path)
   end
 
   def create_startup_script

staging/lib/vcap/staging/plugin/django/plugin.rb
@@ -12,6 +12,7 @@ class DjangoPlugin < StagingPlugin
       create_app_directories
       copy_source_files
       create_startup_script
+      create_stop_script
       create_gunicorn_config
     end
   end
@@ -33,6 +34,11 @@ class DjangoPlugin < StagingPlugin
     generate_startup_script(vars) do
       setup_python_env(REQUIREMENTS)
     end
+  end
+
+  def stop_script
+    vars = environment_hash
+    generate_stop_script(vars)
   end
 
   def create_gunicorn_config

staging/lib/vcap/staging/plugin/java_web/plugin.rb
@@ -29,6 +29,7 @@ class JavaWebPlugin < StagingPlugin
       Tomcat.prepare_insight destination_directory, environment, insight_agent if Tomcat.insight_bound? services
       configure_webapp(webapp_root, self.autostaging_template, environment) unless self.skip_staging(webapp_root)
       create_startup_script
+      create_stop_script
     end
   end
 
@@ -66,6 +67,7 @@ class JavaWebPlugin < StagingPlugin
   end
 
   private
+
   def startup_script
     vars = environment_hash
     vars['CATALINA_OPTS'] = configure_catalina_opts
@@ -88,5 +90,10 @@ fi
 ruby resources/generate_server_xml $PORT
       JAVA
     end
+  end
+
+  def stop_script
+    vars = environment_hash
+    generate_stop_script(vars)
   end
 end

staging/lib/vcap/staging/plugin/node/plugin.rb
@@ -10,6 +10,7 @@ class NodePlugin < StagingPlugin
       create_app_directories
       copy_source_files
       create_startup_script
+      create_stop_script
     end
   end
 
@@ -19,9 +20,15 @@ class NodePlugin < StagingPlugin
   end
 
   private
+
   def startup_script
     vars = environment_hash
     generate_startup_script(vars)
+  end
+
+  def stop_script
+    vars = environment_hash
+    generate_stop_script(vars)
   end
 
   # TODO - I'm fairly sure this problem of 'no standard startup command' is

staging/lib/vcap/staging/plugin/otp_rebar/plugin.rb
@@ -18,6 +18,7 @@ class OtpRebarPlugin < StagingPlugin
       create_app_directories
       copy_source_files
       create_startup_script
+      create_stop_script
       rewrite_libs
       update_vm_args
     end
@@ -44,6 +45,7 @@ class OtpRebarPlugin < StagingPlugin
   end
 
   private
+
   def startup_script
     vars = environment_hash
     generate_startup_script(vars)
@@ -78,6 +80,11 @@ class OtpRebarPlugin < StagingPlugin
         FileUtils.ln_s "#{runtime_dir}/lib/erlang/lib/#{base_lib_name}", lib_name
       end
     end
+  end
+
+  def stop_script
+    vars = environment_hash
+    generate_stop_script(vars)
   end
 
   # We want to alter the VM so that it doesn't want input, and that it doesn't need the double INT to close.

commit 18c63fc3853ad790ec4a1c7e17f1151f687661ba

[dea] Kill apps the right way   This addresses a bug introduced by Eduardo Aceituno in commit 2b56b7f1e4aa2a382f8b7278ff3c0332aee7f958 .   - Correct order of pid in su command   - Less unreadable command substitution   - Log the droplet instance state to confirm that we are killing     slow-starting crashed apps Change-Id: I2c2f5faa969b8e7ccc8e639173e6c2bbefb8b369

dea/lib/dea/agent.rb
@@ -1205,12 +1205,12 @@ module DEA
       if instance[:pid] || [:STARTING, :RUNNING].include?(instance[:state])
         instance[:state] = :STOPPED unless instance[:state] == :CRASHED
         instance[:state_timestamp] = Time.now.to_i
-        stop_cmd = File.join(instance[:dir], 'stop')
-        stop_cmd = "su -c #{stop_cmd} #{username}" if @secure
-        stop_cmd = "#{stop_cmd} #{instance[:pid]} 2> /dev/null"
+        stop_script = File.join(instance[:dir], 'stop')
+        insecure_stop_cmd = "#{stop_script} #{instance[:pid]} 2> /dev/null"
+        stop_cmd = "su -c \"#{insecure_stop_cmd}\" #{username}" if @secure
 
         unless (RUBY_PLATFORM =~ /darwin/ and @secure)
-          @logger.debug("Executing stop script: '#{stop_cmd}'")
+          @logger.debug("Executing stop script: '#{stop_cmd}', instance state is #{instance[:state]}")
           # We can't make 'stop_cmd' into EM.system because of a race with
           # 'cleanup_droplet'
           @logger.debug("Stopping instance PID:#{instance[:pid]}")