ed2d723ed644104c881b948713879f674563b322
Joel D'sa <jdsa@vmware.com>
2012-02-09 19:08:29 -0800

    CC changes to work with the UAA
    Removed unnecessary bcrypt. Token match pattern is less aggressive.
    
    Fix bug to maintain the same code flow for uaa or cc tokens.
    
    Incorporated review comments.
    
    Changed the name of the gem.
    
    Minor bug fix. added config changes.
    
    Changes to support JWT tokens for auth and token
    decoding in the CC.
    
    Change-Id: I1b99082f90c97739558b111ffa58b546231e3ac1

--- cloud_controller/spec/requests/app_create_spec.rb
+++ cloud_controller/spec/requests/app_create_spec.rb
@@ -1,17 +1,33 @@
 require 'spec_helper'
 
 describe "Creating a new App" do
-  before do
-    build_admin_and_user
+
+  shared_examples_for "any request to create a new app" do
+    before do
+      build_admin_and_user
+    end
+
+    it "is successful when given a unique name" do
+      data = { 'name' => random_name, 'staging' => {'model' => 'sinatra', 'stack' => 'ruby18' }}
+      lambda do
+        post app_create_path, nil, headers_for(@user.email, nil, data)
+        response.should redirect_to(app_get_url(data['name']))
+      end.should change(App, :count).by(1)
+    end
+
+    it "fails when given a duplicate name"
   end
 
-  it "is successful when given a unique name" do
-    data = { 'name' => random_name, 'staging' => {'model' => 'sinatra', 'stack' => 'ruby18' }}
-    lambda do
-      post app_create_path, nil, headers_for(@user.email, nil, data)
-      response.should redirect_to(app_get_url(data['name']))
-    end.should change(App, :count).by(1)
+  context "using conventional tokens" do
+    it_should_behave_like "any request to create a new app"
   end
 
-  it "fails when given a duplicate name"
+  context "using jwt tokens" do
+    before :all do
+      CloudSpecHelpers.use_jwt_token = true
+    end
+
+    it_should_behave_like "any request to create a new app"
+  end
+
 end
