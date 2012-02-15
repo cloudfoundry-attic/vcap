04060f5ba1da721fe55478f3f1c60a81f73eac45
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-02-14 16:51:56 -0800

    warden: Give containers their own fs
    Change-Id: I97d1ea8af9adef07486e230012f2b587f6d14599

--- warden/root/linux/skeleton/common.sh
+++ warden/root/linux/skeleton/common.sh
@@ -14,10 +14,17 @@ function chroot() {
 }
 
 function setup_fs() {
+  if [ ! -f fs ]; then
+    dd if=/dev/null of=fs bs=1k seek=512k
+    mkfs.ext4 -q -F fs
+  fi
+
   mkdir -p rootfs ${target}
+  mount -n -o loop fs rootfs
   mount -n -t aufs -o br:rootfs=rw:../../base/rootfs=ro+wh none ${target}
 }
 
 function teardown_fs() {
   umount ${target}
+  umount rootfs
 }
