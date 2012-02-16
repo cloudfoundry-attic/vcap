f251063999b258b1313b3736b352ea3a07e911ca
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-02-15 20:50:05 -0800

    warden: First stop, then remove
    Change-Id: I170bfd484509265b2471acb3374449903a45ad78

--- warden/root/linux/clear.sh
+++ warden/root/linux/clear.sh
@@ -6,7 +6,11 @@ shopt -s nullglob
 cd $(dirname "${0}")
 
 for instance in instances/*; do
-  [ -f ${instance}/stop.sh ] && ${instance}/stop.sh
-  umount ${instance}/union || true
+  [ -f ${instance}/stop.sh ] && ${instance}/stop.sh || true
+done
+
+sleep 0.1
+
+for instance in instances/*; do
   rm -rf ${instance}
 done
