3a4c4fdad9418b32170c45f7d60e299d88ab6369
Jesse Zhang <jessezhang@vmware.com>
2012-02-17 07:02:17 -0800

    [CC] Refactor tests for StagingTaskLog
      Refactored unit tests for StagingTaskLog#fetch_fibered.
    
      Tests: passed unit tests
    
    Change-Id: Ic2b05efba61ebcb25962e8356b2f416a8148eda9

--- cloud_controller/spec/models/staging_task_log_spec.rb
+++ cloud_controller/spec/models/staging_task_log_spec.rb
@@ -23,40 +23,35 @@ describe StagingTaskLog do
   end
 
   describe '#fetch_fibered' do
+    before :each do
+      @redis_mock = mock()
+      @deferrable_mock = EM::DefaultDeferrable.new()
+      @deferrable_mock.stubs(:timeout)
+      @redis_mock.expects(:get).with(@task_key).returns(@deferrable_mock)
+    end
+
     it 'should fetch and decode an existing task result' do
-      redis_mock = mock()
-      d = EM::DefaultDeferrable.new()
-      d.stubs(:timeout)
-      redis_mock.expects(:get).with(@task_key).returns(d)
       Fiber.new do
-        res = StagingTaskLog.fetch_fibered(@task_id, redis_mock)
+        res = StagingTaskLog.fetch_fibered(@task_id, @redis_mock)
         res.should be_instance_of(StagingTaskLog)
       end.resume
-      d.succeed(@task_log.task_log)
+      @deferrable_mock.succeed(@task_log.task_log)
     end
 
     it 'should return nil if no key exists' do
-      redis_mock = mock()
-      d = EM::DefaultDeferrable.new()
-      d.stubs(:timeout)
-      redis_mock.expects(:get).with(@task_key).returns(d)
       Fiber.new do
-        res = StagingTaskLog.fetch_fibered(@task_id, redis_mock)
+        res = StagingTaskLog.fetch_fibered(@task_id, @redis_mock)
         res.should be_nil
       end.resume
-      d.succeed(nil)
+      @deferrable_mock.succeed(nil)
     end
 
     it 'should use the static instance of redis if none is provided' do
-      redis_mock = mock()
-      d = EM::DefaultDeferrable.new()
-      d.stubs(:timeout)
-      redis_mock.expects(:get).with(@task_key).returns(d)
       Fiber.new do
-        StagingTaskLog.redis = redis_mock
+        StagingTaskLog.redis = @redis_mock
         res = StagingTaskLog.fetch_fibered(@task_id)
       end.resume
-      d.succeed(nil)
+      @deferrable_mock.succeed(nil)
     end
   end
 
