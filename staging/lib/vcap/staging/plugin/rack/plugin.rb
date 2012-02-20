6f95737f1e00c5157536bd3869732a4b8f12e6c5
Ken Robertson <ken@invalidlogic.com>
2012-02-20 07:56:16 -0800

    Added Rack framework support

---
+++ staging/lib/vcap/staging/plugin/rack/plugin.rb
@@ -0,0 +1,64 @@
+class RackPlugin < StagingPlugin
+  include GemfileSupport
+  def framework
+    'rack'
+  end
+
+  def stage_application
+    Dir.chdir(destination_directory) do
+      create_app_directories
+      copy_source_files
+      compile_gems
+      create_startup_script
+      create_stop_script
+    end
+  end
+
+  def start_command
+    if uses_bundler?
+      "#{local_runtime} ./rubygems/ruby/#{library_version}/bin/bundle exec #{local_runtime} -S rackup $@ config.ru"
+    else
+      "#{local_runtime} -S rackup $@ config.ru"
+    end
+  end
+
+  private
+  def startup_script
+    vars = environment_hash
+    if uses_bundler?
+      vars['PATH'] = "$PWD/app/rubygems/ruby/#{library_version}/bin:$PATH"
+      vars['GEM_PATH'] = vars['GEM_HOME'] = "$PWD/app/rubygems/ruby/#{library_version}"
+      vars['RUBYOPT'] = '-I$PWD/ruby -rstdsync'
+    else
+      vars['RUBYOPT'] = "-rubygems -I$PWD/ruby -rstdsync"
+    end
+    # PWD here is after we change to the 'app' directory.
+    generate_startup_script(vars) do
+      plugin_specific_startup
+    end
+  end
+
+  def stop_script
+    vars = environment_hash
+    generate_stop_script(vars)
+  end
+
+  def plugin_specific_startup
+    cmds = []
+    cmds << "mkdir ruby"
+    cmds << 'echo "\$stdout.sync = true" >> ./ruby/stdsync.rb'
+    cmds.join("\n")
+  end
+
+  # TODO - I'm fairly sure this problem of 'no standard startup command' is
+  # going to be limited to Sinatra and Node.js. If not, it probably deserves
+  # a place in the sinatra.yml manifest.
+  def detect_main_file
+    file = app_files_matching_patterns.first
+    # TODO - Currently staging exceptions are not handled well.
+    # Convert to using exit status and return value on a case-by-case basis.
+    raise "Unable to determine Rack startup command" unless file
+    file
+  end
+end
+
