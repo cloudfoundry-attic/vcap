952da23e1c1a4bcfbcfb566b42558e11eef9b883
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-02-23 23:48:40 -0800

    warden: Move container pid check inside loop
    There is a race between the start operation being in progress while
    destroy is first executed, where the pid is not yet written when the
    destroy script tries to read it. Checking for the pid file from inside
    the loop body solves this issue.
    
    Change-Id: I2f21cc0a87a731d24c901ce68111d12b1808f758

--- warden/root/linux/skeleton/destroy.sh
+++ warden/root/linux/skeleton/destroy.sh
@@ -5,13 +5,15 @@ set -o errexit
 shopt -s nullglob
 cd $(dirname "${0}")
 
-if [ -f ppid ]; then
-  kill -9 $(cat ppid) 2> /dev/null || true
-  rm -f ppid
-fi
-
 # Wait for the kernel to release resources
-for i in $(seq 20); do
-  [ $(losetup -j fs | wc -l) -eq 0 ] && break
+while [ 1 ]; do
+  if [ -f ppid ]; then
+    kill -9 $(cat ppid) 2> /dev/null || true
+    rm -f ppid
+  fi
+
+  [ $(losetup -j fs | wc -l) -eq 0 ] && exit 0
   sleep 0.1
 done
+
+exit 1
