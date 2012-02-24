28521bff4ea37e9b0f52bf2dc6f77779d4197d59
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-02-23 17:10:28 -0800

    warden: Unshare network namespace for clone utility
    This makes sure that the virtual ethernet pair is created in a new
    network namespace, and thus can never leak back to the host.
    
    x
    
    Change-Id: I294adac40acc397b91e26b09bad396fb1c068dcd

--- warden/root/linux/skeleton/start.sh
+++ warden/root/linux/skeleton/start.sh
@@ -15,7 +15,7 @@ fi
 
 export ROOT_PATH=union
 export ASSET_PATH=$(pwd)
-../../../../src/clone/clone
+unshare -n ../../../../src/clone/clone
 
 ifconfig ${network_iface_host} ${network_gateway_ip} netmask ${network_netmask}
 touch started
