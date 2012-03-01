97f84839fb9e3c0492680d7d2a1027463975067e
Jesse Zhang <jessezhang@vmware.com>
2012-02-29 16:32:26 -0800

    [staging] Dummy commits
      DO NOT REVIEW YET
      Generate CI manifests on the fly (sux)
    
    Change-Id: I1c3ac214b38364b174b3f7ffe1380c8f2f01036e

--- staging/spec/spec_helper.rb
+++ staging/spec/spec_helper.rb
@@ -19,6 +19,13 @@ RSpec.configure do |config|
       puts "Unable to copy staging manifests. Permissions problem?"
       exit 1
     end
+    if ENV['CI_VCAP_RUBY18'] then
+      %w(sinatra rails3 rack).each do |framework|
+        File.open(File.join(STAGING_TEMP, "#{framework}.yml"), "w") do |file|
+          YAML.dump ci_manifests[framework], file
+        end
+      end
+    end
     platform_hash = {}
     File.open(File.join(STAGING_TEMP, 'platform.yml'), 'wb') do |f|
       cache_dir = File.join('/tmp', '.vcap_gems')
@@ -33,4 +40,59 @@ at_exit do
   if File.directory?(STAGING_TEMP)
     FileUtils.rm_r(STAGING_TEMP)
   end
+end
+
+def ci_manifests
+  'rails3' => rails_manifest,
+  'sinatra' => sinatra_manifest,
+  'rack' => rack_manifest,
+end
+def rails_manifest
+  raise ArgumentError unless ENV['CI_VCAP_RUBY18']
+  {
+    "name"=>"rails3",
+    "runtimes"=> [{
+        "ruby18"=> {
+          "version"=>"1.8.7",
+          "description"=>"Ruby 1.8.7",
+          "executable"=>ENV['CI_VCAP_RUBY18'],
+          "default"=>true,
+          "environment"=> {
+            "rails_env"=>"production",
+            "bundle_gemfile"=>nil,
+            "rack_env"=>"production"}
+        }
+      }, {
+        "ruby19"=> {
+          "version"=>"1.9.2p180",
+          "description"=>"Ruby 1.9.2",
+          "executable"=>"ruby",
+          "environment"=> {
+            "rails_env"=>"production",
+            "bundle_gemfile"=>nil,
+            "rack_env"=>"production"}
+        }
+      }
+    ],
+    "app_servers"=> [
+      "thin"=>{
+        "description"=>"Thin",
+        "executable"=>false,
+        "default"=>true}
+    ],
+    "detection"=> [
+      {"config/application.rb"=>true},
+      {"config/environment.rb"=>true}
+    ],
+    "staged_services"=> [
+      {"name"=>"mysql", "version"=>"*"},
+      {"name"=>"postgresql", "version"=>"*"}
+    ]
+  }
+end
+def sinatra_manifest
+  raise NotImplementedError
+end
+def rack_manifest
+  raise NotImplementedError
 end
