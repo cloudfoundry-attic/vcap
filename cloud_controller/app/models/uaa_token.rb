e20abdb7eecd061ce80ed35469835f05ba861cce
Joel D'sa <jdsa@vmware.com>
2012-03-09 16:51:33 -0800

    Changes to the cloud controller to support user creation, deletion and change password via the uaa.
    collected minor config and transition changes
    Add database migration config to dev_setup
    update uaa submodule
    
    Change-Id: If1b112af24de1434bc68ccc1a65bb41a6212c09b

--- cloud_controller/app/models/uaa_token.rb
+++ cloud_controller/app/models/uaa_token.rb
@@ -1,10 +1,17 @@
-
 require "uaa/token_coder"
+require "uaa/token_issuer"
 
 class UaaToken
 
   @uaa_token_coder ||= Cloudfoundry::Uaa::TokenCoder.new(AppConfig[:uaa][:resource_id],
                                                          AppConfig[:uaa][:token_secret])
+
+  @token_issuer ||= Cloudfoundry::Uaa::TokenIssuer.new(AppConfig[:uaa][:url],
+                                                       AppConfig[:uaa][:resource_id],
+                                                       AppConfig[:uaa][:client_secret],
+                                                       "read write password",
+                                                       nil)
+
   class << self
 
     def is_uaa_token?(token)
@@ -27,6 +34,23 @@ class UaaToken
         CloudController.logger.error("Invalid bearer token Message: #{e.message}")
       end
       token_information[:email] if token_information
+    end
+
+    def expired?(access_token)
+      expiry = Cloudfoundry::Uaa::TokenCoder.decode(access_token.split()[1], AppConfig[:uaa][:token_secret])[:expires_at]
+      expiry.is_a?(Integer) && expiry <= Time.now.to_i
+    end
+
+    def access_token
+      if @access_token.nil? || expired?(@access_token)
+        #Get a new one
+        @token_issuer.async = true
+        @token_issuer.trace = true
+        @token_issuer.logger = CloudController.logger
+        @access_token = @token_issuer.client_credentials_grant()
+      end
+      CloudController.logger.debug("access_token #{@access_token}")
+      @access_token
     end
 
   end
