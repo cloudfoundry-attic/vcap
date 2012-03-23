2d2863261e332098cc7761ab3798c5c9f3dd328a
Anfernee Gui <agui@vmware.com>
2012-03-23 10:44:15 -0700

    Update nginx to the 1.2 line
    We need this to support forwarding a chunked response from the backend.
    
    This problem is documented here:
    http://nginx.org/en/docs/faq/chunked_encoding_from_backend.html
    
    Change-Id: Ia9c1a3bc2edbe14139d636a8809236dffcb13536

--- dev_setup/cookbooks/nginx/recipes/default.rb
+++ dev_setup/cookbooks/nginx/recipes/default.rb
@@ -22,59 +22,69 @@ when "ubuntu"
   end
 
   # Lua related packages
-  remote_file File.join("", "tmp", "lua-#{lua_version}.tar.gz") do
+  lua_tgz = "lua-#{lua_version}.tar.gz"
+  lua_dir = "lua-#{lua_version}"
+  remote_file File.join("", "tmp", lua_tgz) do
     owner node[:deployment][:user]
     source lua_source
-    not_if { ::File.exists?(File.join("", "tmp", "lua-#{lua_version}.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", lua_tgz)) }
   end
 
-  remote_file File.join("", "tmp", "lua-cjson-1.0.3.tar.gz") do
+  lua_cjson_tgz = "lua-cjson-1.0.3.tar.gz"
+  lua_cjson_dir = "lua-cjson-1.0.3"
+  remote_file File.join("", "tmp", lua_cjson_tgz) do
     owner node[:deployment][:user]
     source node[:lua][:cjson_source]
-    not_if { ::File.exists?(File.join("", "tmp", "lua-cjson-1.0.3.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", lua_cjson_tgz)) }
   end
 
   # Nginx related packages
-  remote_file File.join("", "tmp", "nginx-#{nginx_version}.tar.gz") do
+  ngx_tgz = "nginx-#{nginx_version}.tar.gz"
+  ngx_dir = "nginx-#{nginx_version}"
+  remote_file File.join("", "tmp", ngx_tgz) do
     owner node[:deployment][:user]
     source nginx_source
-    not_if { ::File.exists?(File.join("", "tmp", "nginx-#{nginx_version}.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", ngx_tgz)) }
   end
 
-  remote_file File.join("", "tmp", "zero_byte_in_cstr_20120315.patch") do
-    owner node[:deployment][:user]
-    source node[:nginx][:patch]
-    not_if { ::File.exists?(File.join("", "tmp", "zero_byte_in_cstr_20120315.patch")) }
-  end
-
-  remote_file File.join("", "tmp", "pcre-8.12.tar.gz") do
+  pcre_tgz = "pcre-8.21.tar.gz"
+  pcre_dir = "pcre-8.21"
+  remote_file File.join("", "tmp", pcre_tgz) do
     owner node[:deployment][:user]
     source node[:nginx][:pcre_source]
-    not_if { ::File.exists?(File.join("", "tmp", "pcre-8.12.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", pcre_tgz)) }
   end
 
-  remote_file File.join("", "tmp", "nginx_upload_module-2.2.0.tar.gz") do
+  ngx_upload_module_tgz = "nginx_upload_module-2.2.0.tar.gz"
+  ngx_upload_module_dir = "nginx_upload_module-2.2.0"
+  remote_file File.join("", "tmp", ngx_upload_module_tgz) do
     owner node[:deployment][:user]
     source node[:nginx][:module_upload_source]
-    not_if { ::File.exists?(File.join("", "tmp", "nginx_upload_module-2.2.0.tar.g")) }
+    not_if { ::File.exists?(File.join("", "tmp", ngx_upload_module_tgz)) }
   end
 
-  remote_file File.join("", "tmp", "headers-more-v0.15rc3.tar.gz") do
+  ngx_headers_more_tgz = "headers-more-v0.15rc3.tar.gz"
+  ngx_headers_more_dir = "agentzh-headers-more-nginx-module-5fac223"
+  remote_file File.join("", "tmp", ngx_headers_more_tgz) do
     owner node[:deployment][:user]
     source node[:nginx][:module_headers_more_source]
-    not_if { ::File.exists?(File.join("", "tmp", "headers-more-v0.15rc3.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", ngx_headers_more_tgz)) }
   end
 
-  remote_file File.join("", "tmp", "devel-kit-v0.2.17rc2.tar.gz") do
+  ngx_devel_kit_tgz = "devel-kit-v0.2.17rc2.tar.gz"
+  ngx_devel_kit_dir = "simpl-ngx_devel_kit-bc97eea"
+  remote_file File.join("", "tmp", ngx_devel_kit_tgz) do
     owner node[:deployment][:user]
     source node[:nginx][:module_devel_kit_source]
-    not_if { ::File.exists?(File.join("", "tmp", "devel-kit-v0.2.17rc2.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", ngx_devel_kit_tgz)) }
   end
 
-  remote_file File.join("", "tmp", "nginx-lua.v0.3.1rc24.tar.gz") do
+  ngx_lua_tgz = "nginx-lua.v0.4.1.tar.gz"
+  ngx_lua_dir = "chaoslawful-lua-nginx-module-204ce2b"
+  remote_file File.join("", "tmp", ngx_lua_tgz) do
     owner node[:deployment][:user]
     source node[:nginx][:module_lua_source]
-    not_if { ::File.exists?(File.join("", "tmp", "nginx-lua.v0.3.1rc24.tar.gz")) }
+    not_if { ::File.exists?(File.join("", "tmp", ngx_lua_tgz)) }
   end
 
   directory nginx_path do
@@ -97,8 +107,8 @@ when "ubuntu"
     cwd File.join("", "tmp")
     user node[:deployment][:user]
     code <<-EOH
-      tar xzf lua-#{lua_version}.tar.gz
-      cd lua-#{lua_version}
+      tar xzf #{lua_tgz}
+      cd #{lua_dir}
       make linux install INSTALL_TOP=#{lua_path}
       EOH
       not_if do
@@ -110,8 +120,8 @@ when "ubuntu"
     cwd File.join("", "tmp")
     user node[:deployment][:user]
     code <<-EOH
-      tar xzf lua-cjson-1.0.3.tar.gz
-      cd lua-cjson-1.0.3
+      tar xzf #{lua_cjson_tgz}
+      cd #{lua_cjson_dir}
       sed 's!^PREFIX ?=.*!PREFIX ?='#{lua_path}'!' Makefile > tmp
       mv tmp Makefile
       make
@@ -126,26 +136,22 @@ when "ubuntu"
     cwd File.join("", "tmp")
     user node[:deployment][:user]
     code <<-EOH
-      tar xzf nginx-#{nginx_version}.tar.gz
-      tar xzf pcre-8.12.tar.gz
-      tar xzf nginx_upload_module-2.2.0.tar.gz
-      tar xzf headers-more-v0.15rc3.tar.gz
-      tar xzf devel-kit-v0.2.17rc2.tar.gz
-      tar xzf nginx-lua.v0.3.1rc24.tar.gz
-      cd nginx-#{nginx_version}
-
-      patch -p0 < ../zero_byte_in_cstr_20120315.patch
-
+      tar xzf #{ngx_tgz}
+      tar xzf #{pcre_tgz}
+      tar xzf #{ngx_upload_module_tgz}
+      tar xzf #{ngx_headers_more_tgz}
+      tar xzf #{ngx_devel_kit_tgz}
+      tar xzf #{ngx_lua_tgz}
+      cd #{ngx_dir}
       LUA_LIB=#{lua_path}/lib LUA_INC=#{lua_path}/include ./configure \
         --prefix=#{nginx_path} \
-        --with-pcre=../pcre-8.12 \
-        --add-module=../nginx_upload_module-2.2.0 \
-        --add-module=../agentzh-headers-more-nginx-module-5fac223 \
-        --add-module=../simpl-ngx_devel_kit-bc97eea \
-        --add-module=../chaoslawful-lua-nginx-module-4d92cb1
+        --with-pcre=../#{pcre_dir} \
+        --add-module=../#{ngx_upload_module_dir} \
+        --add-module=../#{ngx_headers_more_dir} \
+        --add-module=../#{ngx_devel_kit_dir} \
+        --add-module=../#{ngx_lua_dir}
       make
       make install
-
       EOH
   end
 
