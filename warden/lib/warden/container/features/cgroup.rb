c068efe0b068d00e9099ef7f1039d8da197a9db6
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-03-28 11:42:33 -0700

    warden: Move Linux specific setup to root/linux/setup.sh
    Change-Id: I3f7261041e4066a3a964a4782d260c59a30fb805

--- warden/lib/warden/container/features/cgroup.rb
+++ warden/lib/warden/container/features/cgroup.rb
@@ -8,30 +8,6 @@ module Warden
 
       module Cgroup
 
-        def self.included(base)
-          base.extend(ClassMethods)
-        end
-
-        module ClassMethods
-
-          include Spawn
-
-          def setup(config = {})
-            super(config)
-
-            cgroup_path = "/dev/cgroup"
-            cgroup_mounts = IO.readlines("/proc/mounts").
-              map(&:split).
-              select { |e| e[1] == cgroup_path }. # mount point
-              select { |e| e[2] == "cgroup" }     # fs type
-
-            if cgroup_mounts.empty?
-              sh "mkdir -p #{cgroup_path}"
-              sh "mount -t cgroup -o blkio,devices,memory,cpuacct,cpu,cpuset none #{cgroup_path}"
-            end
-          end
-        end
-
         def cgroup_root_path
           File.join("/dev/cgroup", "instance-#{self.handle}")
         end
