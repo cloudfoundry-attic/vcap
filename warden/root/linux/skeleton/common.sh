29dbb32cfe37b5ed6c28728cf8bd65b0b8c86395
Jesse Zhang <jessezhang@vmware.com>
2012-03-27 19:43:32 -0700

    warden: avoid lsb_release when we can
      Directly sourcing /etc/lsb-release in a subshell takes less than 2
    milliseconds whereas lsb-release -cs usually takes more than 25 ms.
    
    Change-Id: Ibf7586291519bdefd4c04b10f962bb1b99638136

--- warden/root/linux/skeleton/common.sh
+++ warden/root/linux/skeleton/common.sh
@@ -25,7 +25,9 @@ function setup_fs() {
   mkdir -p rootfs ${target}
   mount -n -o loop fs rootfs
 
-  codename=$(lsb_release -c -s)
+  codename=$( [ -r /etc/lsb-release ] && . /etc/lsb-release && \
+    [ -n $DISTRIB_CODENAME ] && echo $DISTRIB_CODENAME || \
+    lsb_release -cs || echo unknown)
 
   case "${codename}" in
 
