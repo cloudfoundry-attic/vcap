d29e48156baaa5237c81639ae74068e4cbb2c0d7
Jesse Zhang <jessezhang@vmware.com>
2012-04-01 23:57:19 -0700

    [dev_setup] Cache Erlang tarballs
      To speed up repeated installation on slow links.
    
    Change-Id: I575c76ff167a07c7174d4893c65a6bbc7b9e559d

--- dev_setup/cookbooks/deployment/attributes/default.rb
+++ dev_setup/cookbooks/deployment/attributes/default.rb
@@ -9,3 +9,4 @@ default[:deployment][:domain] = "vcap.me"
 default[:deployment][:log_path] = File.join(deployment[:home], "log")
 default[:deployment][:profile] = File.expand_path(File.join(ENV["HOME"], ".cloudfoundry_deployment_profile"))
 default[:deployment][:local_run_profile] = File.expand_path(File.join(ENV["HOME"], ".cloudfoundry_deployment_local"))
+default[:deployment][:setup_cache] = File.join("", "var", "cache", "dev_setup")
