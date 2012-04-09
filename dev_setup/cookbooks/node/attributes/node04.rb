35afa2b424cc5485a427d76be4e13a07536644a3
Jesse Zhang <jessezhang@vmware.com>
2012-04-09 01:50:50 -0700

    [dev_setup] Cache Node.js tarballs
      Node.js source tarballs are now cached to speed up repeated
    installations.
      Test plan: run dev_setup without cache, with cache, and with a
    corrupted cached tarball.
    
    Change-Id: Id1b8e3626cd9491cfd26e9b338dbbc75eca742cc

--- dev_setup/cookbooks/node/attributes/node04.rb
+++ dev_setup/cookbooks/node/attributes/node04.rb
@@ -6,3 +6,4 @@ default[:node04][:source] = "http://nodejs.org/dist/node-v#{node04[:version]}.ta
 default[:node04][:npm][:version] = "1.0.106"
 default[:node04][:npm][:source] = "http://registry.npmjs.org/npm/-/npm-#{node[:node04][:npm][:version]}.tgz"
 default[:node04][:npm][:path] = File.join(node[:deployment][:home], "deploy", "nodes", "npm-#{node[:node04][:npm][:version]}")
+default[:node][:checksums]["0.4.12"] = "c01af05b933ad4d2ca39f63cac057f54f032a4d83cff8711e42650ccee24fce4"
