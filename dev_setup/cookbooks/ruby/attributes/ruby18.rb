f8b05c3634ff16bef4274029b9c596644039c617
Jesse Zhang <jessezhang@vmware.com>
2012-04-09 01:50:50 -0700

    [dev_setup] Cache ruby tarballs
      Ruby source tarballs are now cached to speed up repeated
    installations.
      Test plan: run dev_setup without cache, with cache, and with a
    corrupted cached tarball.
    
    Change-Id: I3bd5e089d60ac9b51193f80efffa916d63fdba36

--- dev_setup/cookbooks/ruby/attributes/ruby18.rb
+++ dev_setup/cookbooks/ruby/attributes/ruby18.rb
@@ -2,3 +2,4 @@ include_attribute "deployment"
 default[:ruby18][:version] = "1.8.7-p334"
 default[:ruby18][:source]  = "http://ftp.ruby-lang.org//pub/ruby/1.8/ruby-#{ruby18[:version]}.tar.gz"
 default[:ruby18][:path]    = File.join(node[:deployment][:home], "deploy", "rubies", "ruby-#{ruby18[:version]}")
+default[:ruby][:checksums]["1.8.7-p334"] = "68f68d6480955045661fab3be614c504bfcac167d070c6fdbfc9dbe2c5444bc0"
