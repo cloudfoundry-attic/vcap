6dbec6fe68288b0e694bc27e0184db910c5c2d5e
Andrea Campi <andrea.campi@zephirworks.com>
2012-05-05 11:52:45 +0200

    Remove unnecessary depedencies on cookbooks we don't have or need.
    This also fixes uploading the cookbooks to a Chef server, as mentioned
    in issue #204
    
    Change-Id: I3e74af39e86c449904e251fa3ca5241d457c1f1d

--- dev_setup/cookbooks/redis/metadata.rb
+++ dev_setup/cookbooks/redis/metadata.rb
@@ -4,6 +4,3 @@ license          "Apache 2.0"
 description      "Installs/Configures Redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0.0"
-
-depends "env"
-depends "runit"
