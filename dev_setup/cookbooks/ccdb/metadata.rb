bc83dc0b9b967c0627e9ce803a2eaeb380a07e2c
Andrea Campi <andrea.campi@zephirworks.com>
2012-05-05 12:13:34 +0200

    Fix cookbook dependencies for DBs.
    Change-Id: Id767921b2ebd3f96bac5d62c7829e2f94ff0bf9d

--- dev_setup/cookbooks/ccdb/metadata.rb
+++ dev_setup/cookbooks/ccdb/metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures Cloud Controller Database"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+%w{ deployment postgresql }.each do |cb|
+  depends cb
+end
--- dev_setup/cookbooks/uaadb/metadata.rb
+++ dev_setup/cookbooks/uaadb/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures UAA Database"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-depends          "postgresql"
+%w{ deployment postgresql }.each do |cb|
+  depends cb
+end
