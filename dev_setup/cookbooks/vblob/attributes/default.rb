669e794b1a0297e37b86e0a743acec41147b80e8
EagleChen <yanguangchen@rbcon.com>
2012-05-16 20:05:49 -0700

    add vblob service support for dev_setup
    Change-Id: I10bb973e4af3e6d7c7124e834c1ead53256c079e

---
+++ dev_setup/cookbooks/vblob/attributes/default.rb
@@ -0,0 +1,10 @@
+include_attribute "node::node06"
+default[:vblob][:source] = "https://github.com/cloudfoundry/vblob.git"
+default[:vblob][:path] = "/var/vcap/packages/vblob"
+default[:vblob][:commit] = "ec432e6ebaf8b25c2ad900bbbee642f096fef93b"
+
+default[:vblob_node][:capacity] = "200"
+default[:vblob_node][:index] = "0"
+default[:vblob_node][:token] = "changevblobtoken"
+default[:vblob_node][:max_memory] = "128"
+default[:vblob_node][:auth] = "disabled"
