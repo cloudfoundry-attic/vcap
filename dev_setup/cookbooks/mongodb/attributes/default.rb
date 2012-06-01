1ed8440b452bace423b8e0b7705fb7d207e5fea8
Anfernee Gui <agui@vmware.com>
2012-06-01 05:49:03 -0700

    [dev_setup] Fix mongodb on i686
    Change-Id: If7772bdee7959e24253cb9e1ac80ccec76b2d34d

--- dev_setup/cookbooks/mongodb/attributes/default.rb
+++ dev_setup/cookbooks/mongodb/attributes/default.rb
@@ -7,3 +7,12 @@ default[:mongodb_node][:capacity] = "50"
 default[:mongodb_node][:index] = "0"
 default[:mongodb_node][:max_memory] = "128"
 default[:mongodb_node][:token] = "changemongodbtoken"
+
+# The checksums are for mongodb-1.8.1
+if node[:kernel][:machine] == 'x86_64'
+  default[:mongodb][:checksum] = '8f6a58293068e0fb28b463b955f3660f492094e53129fb88af4a7efcfc7995da'
+elsif node[:kernel][:machine] == 'i686'
+  default[:mongodb][:checksum] = '19415154974d62e745977e1bc01e24c0ca3b9d1149881da255315fb4f1cfbf31'
+else
+  Chef::Log.error("Installation of mongodb on #{node[:kernel][:machine]} is not supported")
+end
--- dev_setup/cookbooks/mongodb/recipes/default.rb
+++ dev_setup/cookbooks/mongodb/recipes/default.rb
@@ -8,7 +8,7 @@
 cf_remote_file File.join("", "tmp", "mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb][:version]}.tgz") do
   owner node[:deployment][:user]
   source node[:mongodb][:source]
-  checksum '8f6a58293068e0fb28b463b955f3660f492094e53129fb88af4a7efcfc7995da'
+  checksum node[:mongodb][:checksum]
 end
 
 directory File.join(node[:mongodb][:path], "bin") do
