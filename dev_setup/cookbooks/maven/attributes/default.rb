c3d87445bfac712530fb91ec29dd75630a9723fe
Satoru Chinen <satoru.chinen@gmail.com>
2012-06-05 00:34:10 +0900

    Cache Apache Maven tarball.
    - use cf_remote_file.
    - add checksums (for cf_remote_file).
    - use dev_setup cache.
    
    Change-Id: Ic789e25cee434619d9464d79e0c1bb03596546a8

--- dev_setup/cookbooks/maven/attributes/default.rb
+++ dev_setup/cookbooks/maven/attributes/default.rb
@@ -1,5 +1,6 @@
 include_attribute "deployment"
-default[:maven][:version] = "3.0.4"
-default[:maven][:source]  = "http://mirror.lividpenguin.com/pub/apache/maven/binaries/apache-maven-#{maven[:version]}-bin.tar.gz"
-default[:maven][:base]    = File.join(node[:deployment][:home], "deploy", "maven")
-default[:maven][:path]    = File.join(node[:maven][:base], "apache-maven-#{maven[:version]}")
+default[:maven][:version]  = "3.0.4"
+default[:maven][:source]   = "http://mirror.lividpenguin.com/pub/apache/maven/binaries/apache-maven-#{maven[:version]}-bin.tar.gz"
+default[:maven][:base]     = File.join(node[:deployment][:home], "deploy", "maven")
+default[:maven][:path]     = File.join(node[:maven][:base], "apache-maven-#{maven[:version]}")
+default[:maven][:checksum] = "d35a876034c08cb7e20ea2fbcf168bcad4dff5801abad82d48055517513faa2f"
