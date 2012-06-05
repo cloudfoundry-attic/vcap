74e6440ddc08fbaacc1f4897be393bd5c502f96a
Satoru Chinen <satoru.chinen@gmail.com>
2012-06-06 00:47:01 +0900

    Add timeout attributes to service cookbooks.
    Change-Id: I4e4a491ca22031cbe7a799b5e095c4806165bf07

--- dev_setup/cookbooks/rabbitmq/attributes/default.rb
+++ dev_setup/cookbooks/rabbitmq/attributes/default.rb
@@ -6,5 +6,10 @@ default[:rabbitmq][:source] = "http://www.rabbitmq.com/releases/rabbitmq-server/
 default[:rabbitmq][:plugins] = ["amqp_client", "mochiweb", "rabbitmq-management", "rabbitmq-management-agent", "rabbitmq-mochiweb", "webmachine"]
 default[:rabbitmq][:plugins_source] = "http://www.rabbitmq.com/releases/plugins/v#{rabbitmq[:version]}/"
 
+default[:rabbitmq_gateway][:service][:timeout] = "15"
+default[:rabbitmq_gateway][:node_timeout] = "10"
+
 default[:rabbitmq_node][:index] = "0"
 default[:rabbitmq_node][:token] = "changerabbitmqtoken"
+default[:rabbitmq_node][:op_time_limit] = "6"
+default[:rabbitmq_node][:rabbitmq_start_timeout] = "5"
