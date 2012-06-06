829edba8ba08ec05c29bd21104a97a40fbb2c694
Satoru Chinen <satoru.chinen@gmail.com>
2012-06-07 01:09:07 +0900

    Cache MongoDB tarball.
    - use dev_setup cache.
    
    Change-Id: I4fcd9cacc31428e52e506f106a836972374daa4a

--- dev_setup/cookbooks/mongodb/recipes/default.rb
+++ dev_setup/cookbooks/mongodb/recipes/default.rb
@@ -5,7 +5,8 @@
 # Copyright 2011, VMware
 #
 
-cf_remote_file File.join("", "tmp", "mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb][:version]}.tgz") do
+mongodb_tarball_path = File.join(node[:deployment][:setup_cache], "mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb][:version]}.tgz")
+cf_remote_file mongodb_tarball_path do
   owner node[:deployment][:user]
   source node[:mongodb][:source]
   checksum node[:mongodb][:checksum]
@@ -23,7 +24,7 @@ bash "Install Mongodb" do
   cwd File.join("", "tmp")
   user node[:deployment][:user]
   code <<-EOH
-  tar xvzf mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb][:version]}.tgz
+  tar xvzf #{mongodb_tarball_path}
   cd mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb][:version]}
   cp #{File.join("bin", "*")} #{File.join(node[:mongodb][:path], "bin")}
   EOH
