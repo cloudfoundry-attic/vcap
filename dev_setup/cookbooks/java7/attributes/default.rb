0712e257aac07aa8ee68a51010e35a5f01ee6b8f
Thomas Risberg <trisberg@vmware.com>
2012-06-07 12:33:15 -0400

    Adding Java 7 support
     - modifying chef scripts to support new Java 7 runtime
    
     - adding Java 7 as runtime for standalone apps
    
     - adding Java 7 as runtime for jvm based web frameworks
    
    Change-Id: I33c74cab9d4f530a80dd941d77ce986435a04cad

---
+++ dev_setup/cookbooks/java7/attributes/default.rb
@@ -0,0 +1,10 @@
+include_attribute "deployment"
+default[:java7][:version] = "1.7.0"
+default[:java7][:version_flag] = "-version"
+default[:java7][:oracle_home] = "/usr/lib/jvm/java-7-oracle/jre"
+default[:java7][:openjdk_home] = "/usr/lib/jvm/java-7-openjdk/jre"
+default[:java7][:bin_path] = "bin"
+default[:java7][:exec] = "java"
+default[:java7][:path] = default[:java7][:openjdk_home] + "/" + default[:java7][:bin_path]
+default[:java7][:java_home] = default[:java7][:openjdk_home]
+default[:java7][:available?] = false
