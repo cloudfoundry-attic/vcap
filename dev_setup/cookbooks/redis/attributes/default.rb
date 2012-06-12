a519ef2415a3f6acf899d6081635c7d3c8a0304e
Ben McCann <ben@benmccann.com>
2012-06-11 17:46:40 -0700

    Allow vcap to be installed on Ubuntu 12.04
    Change-Id: I6e5c23879a618016f93c0bffd492ae7f531505e2

--- dev_setup/cookbooks/redis/attributes/default.rb
+++ dev_setup/cookbooks/redis/attributes/default.rb
@@ -1,11 +1,11 @@
 include_attribute "deployment"
-default[:redis][:version] = "2.2.4"
+default[:redis][:version] = "2.2.15"
 default[:redis][:path] = File.join(node[:deployment][:home], "deploy", "redis")
 default[:redis][:runner] = node[:deployment][:user]
 default[:redis][:port] = 6379
 default[:redis][:password] = "redis"
 
-default[:redis][:checksum] = "6d612b28137c926fb6b668fd85d25862469f9755af4e15f1b37cbe6f88882b32"
+default[:redis][:checksum] = "4143b7fab809c5fe586265b4f792f346206a3a8082bbf79f70081a0538bab3cb"
 
 default[:redis_node][:capacity] = "200"
 default[:redis_node][:index] = "0"
