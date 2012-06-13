12328092dd0691a3ab88f260803f11d59402d4a5
Haipeng Wu <hwu@rbcon.com>
2012-06-13 11:03:42 +0800

    [dev_setup] Replace public web links with cf blobs
    Dev_setup needs to download tarballs from public web site, this
    leads to setup failure when one of those public sites is broken.
    
    This change list makes dev_setup download tarballs from cf blobs,
    dev_setup will be much more stable.
    
    Change-Id: I42ce1cc96d7946fc3606986c2bf27e2a38a6ec69

--- dev_setup/cookbooks/rabbitmq/recipes/default.rb
+++ dev_setup/cookbooks/rabbitmq/recipes/default.rb
@@ -10,18 +10,11 @@ case node['platform']
 when "ubuntu"
   package "erlang-nox"
 
-  remote_file File.join("", "tmp", "rabbitmq-server-#{node[:rabbitmq][:version_full]}.tar.gz") do
+  rabbitmq_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-server-with-plugins-#{node[:rabbitmq][:version_full]}.tar.gz")
+  cf_remote_file rabbitmq_tarball_path do
     owner node[:deployment][:user]
-    source node[:rabbitmq][:source]
-    not_if { ::File.exists?(File.join("", "tmp", "rabbitmq-server-#{node[:rabbitmq][:version_full]}.tar.gz")) }
-  end
-
-  node[:rabbitmq][:plugins].each do |plugin_name|
-    remote_file File.join("", "tmp", "#{plugin_name}-#{node[:rabbitmq][:version]}.ez") do
-      owner node[:deployment][:user]
-      source "#{node[:rabbitmq][:plugins_source]}#{plugin_name}-#{node[:rabbitmq][:version]}.ez"
-      not_if { ::File.exists?(File.join("", "tmp", "#{plugin_name}-#{node[:rabbitmq][:version]}.ez")) }
-    end
+    id node[:rabbitmq][:id]
+    checksum node[:rabbitmq][:checksum]
   end
 
   directory "#{node[:rabbitmq][:path]}" do
@@ -34,27 +27,12 @@ when "ubuntu"
     cwd File.join("", "tmp")
     user node[:deployment][:user]
     code <<-EOH
-    tar xzf rabbitmq-server-#{node[:rabbitmq][:version_full]}.tar.gz
+    tar xzf #{rabbitmq_tarball_path}
     cd rabbitmq_server-#{node[:rabbitmq][:version]}
     cp -rf * #{node[:rabbitmq][:path]}
     EOH
-    not_if do
-      ::File.exists?(File.join(node[:rabbitmq][:path], "sbin", "rabbitmq-server"))
-    end
   end
 
-  node[:rabbitmq][:plugins].each do |plugin_name|
-    bash "Install RabbitMQ #{plugin_name} plugin" do
-      cwd File.join("", "tmp")
-      user node[:deployment][:user]
-      code <<-EOH
-      cp -f "#{plugin_name}-#{node[:rabbitmq][:version]}.ez" #{File.join(node[:rabbitmq][:path], "plugins")}
-      EOH
-      not_if do
-        ::File.exists?(File.join(node[:rabbitmq][:path], "plugins", "#{plugin_name}-#{node[:rabbitmq][:version]}.ez"))
-      end
-    end
-  end
 else
   Chef::Log.error("Installation of rabbitmq packages not supported on this platform.")
 end
