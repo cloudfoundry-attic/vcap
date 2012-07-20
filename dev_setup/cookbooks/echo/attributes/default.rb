8b4b17bfc32900e2d7a5aa28f8510131ffd9c331
Jeff Li <jeffli@rbcon.com>
2012-07-20 06:25:35 -0600

    Fix the bug when dev_setup running in 32-bit boxes
    Because currently echo server uses a third party binary file which is
    architecture dependent, it will only work in x86_64 platform at the
    moment.This patch removes the third party binary and uses bash script
    instead to make it architecture indepentdent.
    
    Change-Id: I1477c6cd0ce9ea8f629fbb10900b923051803aac

--- dev_setup/cookbooks/echo/attributes/default.rb
+++ dev_setup/cookbooks/echo/attributes/default.rb
@@ -6,7 +6,7 @@ default[:echo_node][:capacity] = "100"
 default[:echo_node][:index] = "0"
 default[:echo_node][:token] = "changeechotoken"
 
-default[:echo_server][:id] = "eyJvaWQiOiI0ZTRlNzhiY2E2MWUxMjEyMDRlNGU4NmVlYmU1OTEwNGZmMDQ3%0AYTExOGEyNiIsInNpZyI6Im9wNVBMcVZaV1lFVVZ2bkhpaDFORnBVSENjMD0i%0AfQ==%0A"
-default[:echo_server][:checksum] = "a1a3e89ae72ceb8f05106ad0666e4638077591090f28797ec240ded4956b610e"
+default[:echo_server][:id] = "eyJvaWQiOiI0ZTRlNzhiY2ExMWUxMjEwMDRlNGU3ZDUxMWY4MjEwNTAwOTA0%0ANGIxMDRkMCIsInNpZyI6Ikd2RFlpUlh2bEhrdnoxT3pyTVJhYlhUcjJyMD0i%0AfQ==%0A"
+default[:echo_server][:checksum] = "a4d5976dfff5d0c9e14fd6eaaa587fcc2543f18c88e3f28960cd18d04c1fa7c9"
 default[:echo_server][:path] = File.join(node[:deployment][:home], "deploy", "echoserver")
 default[:echo_server][:port] = 5002
