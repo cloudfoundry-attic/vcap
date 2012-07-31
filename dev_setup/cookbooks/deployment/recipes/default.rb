c5b385b5d8adb9e3971bc2c938a6aa65d8f949e9
Frank Lu <flu@rbcon.com>
2012-07-31 10:12:03 +0000

    [dev_setup] refactor import_from_data api for lifecycle feature
    Change-Id: Iafe1123d430c3aa343ac9a26081793f8707f0cc1

--- dev_setup/cookbooks/deployment/recipes/default.rb
+++ dev_setup/cookbooks/deployment/recipes/default.rb
@@ -11,6 +11,7 @@ node[:uaadb][:host] ||= cf_local_ip
 node[:postgresql][:host] ||= cf_local_ip
 node[:redis_resque][:host] ||= cf_local_ip
 node[:vcap_redis][:host] ||= cf_local_ip
+node[:serialization_data_server][:host] ||= cf_local_ip
 
 [
   node[:deployment][:home], File.join(node[:deployment][:home], "deploy"),
