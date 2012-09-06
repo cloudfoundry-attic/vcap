ed92dd9b38c5cd6e4e7b7a4be704b22b4096b636
Haipeng Wu <hwu@rbcon.com>
2012-09-05 20:41:06 -0600

    [dev_setup] add warden support
    wardenize for below service components:
    vblob
    redis
    mongodb
    rabbitmq
    
    Change-Id: I2370ac8785b321e47534553474310f8cb2bdca90

--- dev_setup/cookbooks/redis/attributes/default.rb
+++ dev_setup/cookbooks/redis/attributes/default.rb
@@ -3,12 +3,8 @@ include_attribute "deployment"
 include_attribute "backup"
 include_attribute "service_lifecycle"
 
-default[:redis][:supported_versions] = {
-        "2.2" => "2.2.15",
-}
-default[:redis][:version_aliases] = {
-        "current" => "2.2",
-}
+default[:redis][:supported_versions] = { "2.2" => "2.2.15" }
+default[:redis][:version_aliases] = { "current" => "2.2" }
 default[:redis][:default_version] = "2.2"
 
 default[:redis][:path] = File.join(node[:deployment][:home], "deploy", "redis")
@@ -17,8 +13,12 @@ default[:redis][:port] = 6379
 default[:redis][:password] = "redis"
 default[:redis][:expire] = 60
 
-default[:redis][:id] = "eyJzaWciOiJ0akRiejV6Mk9aT2ZLcHlqdHJCaW1QbnJrVUk9Iiwib2lkIjoi%0ANGU0ZTc4YmNhNDFlMTIyMjA0ZTRlOTg2M2QwNzYzMDUwMTlmOGY5YzVkZjci%0AfQ==%0A"
-default[:redis][:checksum] = "4143b7fab809c5fe586265b4f792f346206a3a8082bbf79f70081a0538bab3cb"
+default[:redis][:id] = {
+  "2.2.15" => "eyJzaWciOiJ0akRiejV6Mk9aT2ZLcHlqdHJCaW1QbnJrVUk9Iiwib2lkIjoi%0ANGU0ZTc4YmNhNDFlMTIyMjA0ZTRlOTg2M2QwNzYzMDUwMTlmOGY5YzVkZjci%0AfQ==%0A"
+}
+default[:redis][:checksum] = {
+  "2.2.15" => "4143b7fab809c5fe586265b4f792f346206a3a8082bbf79f70081a0538bab3cb"
+}
 
 default[:redis_gateway][:service][:timeout] = "15"
 default[:redis_gateway][:node_timeout] = "5"
--- dev_setup/cookbooks/redis/recipes/default.rb
+++ dev_setup/cookbooks/redis/recipes/default.rb
@@ -5,48 +5,56 @@
 # Copyright 2012, VMware
 #
 
+# deploy redis in warden
+template "redis_startup.sh" do
+   path File.join(node[:warden][:rootfs_path], "usr", "bin", "redis_startup.sh")
+   source "redis_startup.sh.erb"
+   mode 0755
+end
+
 node[:redis][:supported_versions].each do |version, install_version|
   #TODO, need more refine to actually support mutiple versions
-  Chef::Log.info("Building redis version: #{version} - #{install_version}")
+  Chef::Log.info("Building redis version: #{version}")
 
   cf_remote_file File.join(node[:deployment][:setup_cache], "redis-#{install_version}.tar.gz") do
     owner node[:deployment][:user]
-    id node[:redis][:id]
-    checksum node[:redis][:checksum]
+    id node[:redis][:id]["#{install_version}"]
+    checksum node[:redis][:checksum]["#{install_version}"]
   end
 
-  directory "#{node[:redis][:path]}" do
-    owner node[:deployment][:user]
-    group node[:deployment][:group]
-    mode "0755"
-  end
-
-  %w[bin etc var].each do |dir|
-    directory File.join(node[:redis][:path], dir) do
-      owner node[:deployment][:user]
-      group node[:deployment][:group]
-      mode "0755"
-      recursive true
-      action :create
-    end
-  end
-
-  bash "Install Redis" do
+  bash "Install Redis #{version}" do
     cwd File.join("", "tmp")
     user node[:deployment][:user]
     code <<-EOH
-  tar xzf #{File.join(node[:deployment][:setup_cache], "redis-#{install_version}.tar.gz")}
-  cd redis-#{install_version}
-  make
-  cd src
-  install redis-benchmark redis-cli redis-server redis-check-dump redis-check-aof #{File.join(node[:redis][:path], "bin")}
-  EOH
+    tar xzf #{File.join(node[:deployment][:setup_cache], "redis-#{install_version}.tar.gz")}
+    cd redis-#{install_version}
+    make
+    sudo cp src/redis-server #{File.join(node[:warden][:rootfs_path], "usr", "bin", "redis-server-#{version}")}
+    EOH
   end
+end
 
-  template File.join(node[:redis][:path], "etc", "redis.conf") do
-    source "redis.conf.erb"
-    mode 0600
+# deploy redis local
+directory "#{node[:redis][:path]}" do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+end
+
+%w[bin etc var].each do |dir|
+  directory File.join(node[:redis][:path], dir) do
     owner node[:deployment][:user]
     group node[:deployment][:group]
+    mode "0755"
+    recursive true
+    action :create
   end
+end
+
+bash "Install Redis in local" do
+  user node[:deployment][:user]
+  code <<-EOH
+    cd /tmp/redis-2.2.15/src
+    install redis-benchmark redis-cli redis-server redis-check-dump redis-check-aof #{File.join(node[:redis][:path], "bin")}
+  EOH
 end
