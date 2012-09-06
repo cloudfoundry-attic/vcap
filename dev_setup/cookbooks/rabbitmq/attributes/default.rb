e0be40625badbbf7c66e2506f5b22e859a3155fa
Haipeng Wu <hwu@rbcon.com>
2012-09-05 20:41:06 -0600

    [dev_setup] add warden support
    wardenize for below service components:
    vblob
    redis
    mongodb
    rabbitmq
    
    Change-Id: I2370ac8785b321e47534553474310f8cb2bdca90

--- dev_setup/cookbooks/rabbitmq/attributes/default.rb
+++ dev_setup/cookbooks/rabbitmq/attributes/default.rb
@@ -9,8 +9,15 @@ default[:rabbitmq][:version_aliases] = {
 default[:rabbitmq][:default_version] = "2.4"
 
 default[:rabbitmq][:path] = File.join(node[:deployment][:home], "deploy", "rabbitmq")
-default[:rabbitmq][:id] = "eyJzaWciOiJOTWRVQm01RHU3c1RXcHF4dUovUm93S1c0UUU9Iiwib2lkIjoi%0ANGU0ZTc4YmNhNDFlMTIxMjA0ZTRlODZlZTUzOTIxMDUwMTlmYTU0YWRkZTEi%0AfQ==%0A"
-default[:rabbitmq][:checksum] = "0a87dfe4489b0ddabfee7306536530934a4d4518ef0821e08634c7a07d4cf732"
+default[:rabbitmq][:id] = {
+  "2.4.1" => "eyJzaWciOiJVOUo4bW9Kci9tSlY2VW84WG9sa1NVMEFMb0k9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMTFlMTIxMDA0ZTRlN2Q1MTFmODIxMDUwNWE5N2EwZjI4MDUi%0AfQ==%0A"
+}
+default[:rabbitmq][:checksum] = {
+  "2.4.1" => "1cc8dbe3a54b7ef454adb3b6f3bce2f4c798e45ab1608b9600b2dc01a04e5858"
+}
+
+default[:rabbitmq][:erlang_id] = "eyJzaWciOiJIb2RpNmdVdnp0S2VHSGRtRUxYQ3dGWnc3WHM9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMzFlMTIxMDA0ZTRlN2Q1MTQ3NDVmMDUwNTljZDgxMmY5YWMi%0AfQ==%0A"
+default[:rabbitmq][:erlang_checksum] = "4c72446596803e8a41ec06e7bee5e840d81756f25613bbe3042609c7d0cb0d6b"
 
 default[:rabbitmq_gateway][:service][:timeout] = "15"
 default[:rabbitmq_gateway][:node_timeout] = "10"
