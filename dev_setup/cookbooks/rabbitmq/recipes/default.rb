7f7862b004cfc2617af8902c1decbcbdd0100179
Haipeng Wu <hwu@rbcon.com>
2012-09-05 20:41:06 -0600

    [dev_setup] add warden support
    wardenize for below service components:
    vblob
    redis
    mongodb
    rabbitmq
    
    Change-Id: I2370ac8785b321e47534553474310f8cb2bdca90

--- dev_setup/cookbooks/rabbitmq/recipes/default.rb
+++ dev_setup/cookbooks/rabbitmq/recipes/default.rb
@@ -6,38 +6,46 @@
 #
 #
 
+template "rabbitmq_startup.sh" do
+   path File.join(node[:warden][:rootfs_path], "usr", "bin", "rabbitmq_startup.sh")
+   source "rabbitmq_startup.sh.erb"
+   mode 0755
+end
+
+rabbitmq_erlang_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-erlang.tar.gz")
+cf_remote_file rabbitmq_erlang_tarball_path do
+  owner node[:deployment][:user]
+  id node[:rabbitmq][:erlang_id]
+  checksum node[:rabbitmq][:erlang_checksum]
+end
+
+bash "Install Erlang for RabbitMQ" do
+  code <<-EOH
+  cd #{node[:warden][:rootfs_path]}/var/vcap/packages
+  tar xzf #{rabbitmq_erlang_tarball_path}
+  EOH
+end
+
 node[:rabbitmq][:supported_versions].each do |version, install_version|
   #TODO, need more refine to actually support mutiple versions
   Chef::Log.info("Building rabbitmq version: #{version} - #{install_version}")
 
   case node['platform']
   when "ubuntu"
-    package "erlang-nox"
-    install_path = File.join(node[:deployment][:home], "deploy", "rabbitmq", install_version)
-    source_file_id, source_file_checksum = id_and_checksum_for_rabbitmq_version(install_version)
-    rabbitmq_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-server-with-plugins-generic-unix-#{install_version}.tar.gz")
+    rabbitmq_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-server-with-plugins-#{install_version}.tar.gz")
     cf_remote_file rabbitmq_tarball_path do
       owner node[:deployment][:user]
-      id source_file_id
-      checksum source_file_checksum
-    end
-
-    directory install_path do
-      owner node[:deployment][:user]
-      group node[:deployment][:group]
-      mode "0755"
-      recursive true
-      action :create
+      id node[:rabbitmq][:id]["#{install_version}"]
+      checksum node[:rabbitmq][:checksum]["#{install_version}"]
     end
 
     bash "Install RabbitMQ #{install_version} As #{version}" do
       cwd File.join("", "tmp")
-      user node[:deployment][:user]
       code <<-EOH
-    tar xzf #{rabbitmq_tarball_path}
-    cd rabbitmq_server-#{install_version}
-    cp -rf * #{install_path}
-    EOH
+      tar xzf #{rabbitmq_tarball_path}
+      mkdir -p  #{node[:warden][:rootfs_path]}/var/vcap/packages/rabbitmq/#{version}
+      cp -rf rabbitmq/* #{node[:warden][:rootfs_path]}/var/vcap/packages/rabbitmq/#{version}
+      EOH
     end
 
   else
