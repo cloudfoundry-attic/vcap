8125c891a766b2f6ae204d782507de68b161d965
Ben McCann <ben@benmccann.com>
2012-09-21 02:33:04 -0700

    Move the postgresql setup to the postgresql cookbook
    Change-Id: I31ea36ce27e548908412ebfb4e12065874dede70

--- dev_setup/cookbooks/postgresql/recipes/default.rb
+++ dev_setup/cookbooks/postgresql/recipes/default.rb
@@ -20,6 +20,33 @@ end
 case node['platform']
 when "ubuntu"
 
+  machine =  node[:kernel][:machine]
+  libpq_deb_path = File.join(node[:deployment][:setup_cache], "libpq5_9.2.deb")
+  cf_remote_file libpq_deb_path do
+    owner node[:deployment][:user]
+    id node[:postgresql][:id][:libpq]["#{machine}"]
+    checksum node[:postgresql][:checksum][:libpq]["#{machine}"]
+  end
+
+  libpq_dev_deb_path = File.join(node[:deployment][:setup_cache], "libpq-dev_9.2.deb")
+  cf_remote_file libpq_dev_deb_path do
+    owner node[:deployment][:user]
+    id node[:postgresql][:id][:libpq_dev]["#{machine}"]
+    checksum node[:postgresql][:checksum][:libpq_dev]["#{machine}"]
+  end
+
+  bash "Install libpq" do
+    code <<-EOH
+    dpkg -i #{libpq_deb_path}
+    EOH
+  end
+
+  bash "Install libpq-dev" do
+    code <<-EOH
+    dpkg -i #{libpq_dev_deb_path}
+    EOH
+  end
+
   %w[libc6 libcomerr2 libgssapi-krb5-2 libkrb5-3 libldap-2.4-2 libpam0g libssl0.9.8 libxml2 tzdata ssl-cert locales libedit2 zlib1g].each do |pkg|
     package pkg
   end
