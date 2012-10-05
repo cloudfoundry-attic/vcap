4c750505fcf9f3e46b639ec21f77062e114c2b35
Ben McCann <ben@benmccann.com>
2012-10-05 13:38:57 -0700

    Move the postgresql setup to the postgresql cookbook
    Change-Id: I52df22677da58a6ef36f2b082086e8cc4d208a89

--- dev_setup/cookbooks/essentials/recipes/default.rb
+++ dev_setup/cookbooks/essentials/recipes/default.rb
@@ -14,33 +14,6 @@
   end
 end
 
-machine =  node[:kernel][:machine]
-libpq_deb_path = File.join(node[:deployment][:setup_cache], "libpq5_9.2.deb")
-cf_remote_file libpq_deb_path do
-  owner node[:deployment][:user]
-  id node[:postgresql][:id][:libpq]["#{machine}"]
-  checksum node[:postgresql][:checksum][:libpq]["#{machine}"]
-end
-
-libpq_dev_deb_path = File.join(node[:deployment][:setup_cache], "libpq-dev_9.2.deb")
-cf_remote_file libpq_dev_deb_path do
-  owner node[:deployment][:user]
-  id node[:postgresql][:id][:libpq_dev]["#{machine}"]
-  checksum node[:postgresql][:checksum][:libpq_dev]["#{machine}"]
-end
-
-bash "Install libpq" do
-  code <<-EOH
-  dpkg -i #{libpq_deb_path}
-  EOH
-end
-
-bash "Install libpq-dev" do
-  code <<-EOH
-  dpkg -i #{libpq_dev_deb_path}
-  EOH
-end
-
 if node[:deployment][:profile]
   file node[:deployment][:profile] do
     owner node[:deployment][:user]
