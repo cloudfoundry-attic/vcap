186ec8331b3650a06559f51ac19ec5acd99dcba2
Maria Shaldybina <mariash@vmware.com>
2012-11-12 15:27:09 -0800

    [dev_setup] Update bundler to 1.2.1
    Change-Id: Ia487840a9f3b9b25497643c7f00adaed18cefb50

--- dev_setup/cookbooks/ruby/attributes/default.rb
+++ dev_setup/cookbooks/ruby/attributes/default.rb
@@ -7,4 +7,4 @@ default[:ruby][:checksums]["1.9.2-p180"] = "9027a5abaaadc2af85005ed74aeb628ce232
 default[:rubygems][:version] = "1.8.24"
 default[:rubygems][:id] = "eyJzaWciOiJIUk51OEJpN2pkdTVDTmVKUTlZZ1N5NGxraHc9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMzFlMTIxMDA0ZTRlN2Q1MTQ3NDVmMDUwMTlmMzU5MmU4ZDki%0AfQ==%0A"
 default[:rubygems][:checksum] = "4b61fa51869b3027bcfe67184b42d2e8c23fa6ab17d47c5c438484b9be2821dd"
-default[:ruby][:bundler][:version] = "1.1.3"
+default[:ruby][:bundler][:version] = "1.2.1"
