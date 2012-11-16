0f12fb8b0af23fa6376abc2f6e6b0099a8486157
Harshawardhan Gadgil <hgadgil@rbcon.com>
2012-11-16 12:13:44 -0800

    Dev-setup for multiple redis version
    Conflicts:
    
    	dev_setup/cookbooks/redis/templates/default/redis_node.yml.erb
    	dev_setup/cookbooks/redis/templates/default/redis_worker.yml.erb
    
    Change-Id: I19d2769271c7d656acfbc0ddb13f5935a53f2b98

---
+++ dev_setup/cookbooks/redis/libraries/redis.rb
@@ -0,0 +1,19 @@
+module CloudFoundryRedis
+
+  def id_and_checksum_for_redis_version(version)
+    id = ''
+    checksum = ''
+    id_for_version = node[:redis][:id]["#{version}"]
+    checksum_for_version = node[:redis][:checksum]["#{version}"]
+
+    if id_for_version.nil? || checksum_for_version.nil?
+      Chef::Log.error("Unsupported version: #{version}")
+    end
+
+    [id_for_version, checksum_for_version]
+  end
+end
+
+class Chef::Recipe
+  include CloudFoundryRedis
+end
