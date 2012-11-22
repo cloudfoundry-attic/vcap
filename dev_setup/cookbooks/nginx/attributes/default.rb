5aa9fb63db99e13043c42d4d39bc37f145b6ae99
Haipeng Wu <hwu@rbcon.com>
2012-11-21 20:24:34 -0700

    [dev_setup] apply the upload patch for nginx
    Change-Id: I5085c74deb564062911324a68f275631cd0be830

--- dev_setup/cookbooks/nginx/attributes/default.rb
+++ dev_setup/cookbooks/nginx/attributes/default.rb
@@ -8,6 +8,7 @@ default[:nginx][:module_upload_id]  = "eyJzaWciOiJNU2VoeXVtVTZabDQxaHNLT2xLTFhka
 default[:nginx][:module_headers_more_id]  = "eyJzaWciOiJHNmZUd092Wk03MTBIdWZGbTZsdVhlUkkvQkk9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMjFlMTIxMDA0ZTRlN2Q1MTFmNTUzMDUwMThmM2I2YTEzMGQi%0AfQ==%0A"
 default[:nginx][:module_devel_kit_id]  = "eyJzaWciOiJPZ0dPL3BBbmszOS96MWYvWFNadm11bGZDNFk9Iiwib2lkIjoi%0ANGU0ZTc4YmNhNTFlMTIyMDA0ZTRlOGVjNjg0MDc3MDUwMThmM2FkYmExZmMi%0AfQ==%0A"
 default[:nginx][:module_lua_id]  = "eyJzaWciOiIrODdSUXRTbUFINm5Sd3NNcFc2YlFDSVNPUXM9Iiwib2lkIjoi%0ANGU0ZTc4YmNhNTFlMTIyMDA0ZTRlOGVjNjg0MDc3MDUwMThmMmQ2ZGVhZTci%0AfQ==%0A"
+default[:nginx][:upload_module_patch_id] = "eyJzaWciOiJSR3o1Z2JqZW9LZ2Vacjc3MW0wMyt2REhnSW89Iiwib2lkIjoi%0ANGU0ZTc4YmNhNjFlMTIxMjA0ZTRlODZlZWJlNTkxMDUwYWQ4Yjg1OTM2NTgi%0AfQ==%0A"
 default[:nginx][:path]    = File.join(node[:deployment][:home], "deploy", "nginx", "nginx-#{nginx[:version]}")
 default[:nginx][:vcap_log] = File.join(node[:deployment][:home], "sys", "log", "vcap.access.log")
 default[:nginx][:checksums][:source] = "12e28efb9a54452fa6e579e08ce7c864e49d6ea6104cc2b3de5a4416ead90593"
@@ -17,6 +18,7 @@ default[:nginx][:checksums][:module_upload_source] = "b1c26abe0427180602e257627b
 default[:nginx][:checksums][:module_headers_more_source] = "10587007913805a5193e3104e81fd180a5c52bf7c3f5a5746081660916619bde"
 default[:nginx][:checksums][:module_devel_kit_source] = "bf5540d76d1867b4411091f16c6c786fd66759099c59483c76c68434020fdb02"
 default[:nginx][:checksums][:module_lua_source] = "249042d34a44dddd0ab06a37066fc9a09b692cef5f317d5e86f18d838bb0323f"
+default[:nginx][:checksums][:upload_module_patch] = "fe3d14b255a129e81d58223fcf3c5f3bfc31f2d8b0f6e6b6e3a15fb71975b7ad"
 
 default[:lua][:version] = "5.1.4"
 default[:lua][:simple_version] = lua[:version].match(/\d+\.\d+/).to_s # something like 5.1
