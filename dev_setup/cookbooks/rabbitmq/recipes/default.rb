88e91b829e20c478ba2067098a4a24e781e88c1b
andyzh <andyzhang@rbcon.com>
2012-11-26 07:27:10 -0700

    support rabbit for multi version
    Change-Id: I47142f92c6b4d51c433fdd7ba9bbf761d2e7b9fe

--- dev_setup/cookbooks/rabbitmq/recipes/default.rb
+++ dev_setup/cookbooks/rabbitmq/recipes/default.rb
@@ -13,27 +13,28 @@ node[:rabbitmq][:supported_versions].each do |version, install_version|
   case node['platform']
   when "ubuntu"
     package "erlang-nox"
-
+    install_path = File.join(node[:deployment][:home], "deploy", "rabbitmq", install_version)
+    source_file_id, source_file_checksum = id_and_checksum_for_rabbitmq_version(install_version)
     rabbitmq_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-server-with-plugins-generic-unix-#{install_version}.tar.gz")
     cf_remote_file rabbitmq_tarball_path do
       owner node[:deployment][:user]
-      id node[:rabbitmq][:id]
-      checksum node[:rabbitmq][:checksum]
+      id source_file_id
+      checksum source_file_checksum
     end
 
-    directory "#{node[:rabbitmq][:path]}" do
+    directory install_path do
       owner node[:deployment][:user]
       group node[:deployment][:group]
       mode "0755"
     end
 
-    bash "Install RabbitMQ" do
+    bash "Install RabbitMQ #{install_version} As #{version}" do
       cwd File.join("", "tmp")
       user node[:deployment][:user]
       code <<-EOH
     tar xzf #{rabbitmq_tarball_path}
     cd rabbitmq_server-#{install_version}
-    cp -rf * #{node[:rabbitmq][:path]}
+    cp -rf * #{install_path}
     EOH
     end
 
