6ae33397e5d0f1dd9b976028e5b79405265ae787
Haipeng Wu <hwu@rbcon.com>
2012-12-18 03:52:37 -0700

    Upgrade vblob to latest in services_ng branch
    Change-Id: Ifed58ba5d7d525216fca44fdb5f8732226eca00d

--- dev_setup/cookbooks/vblob/recipes/default.rb
+++ dev_setup/cookbooks/vblob/recipes/default.rb
@@ -21,28 +21,49 @@ bash "prepare to install vBlob" do
   EOH
 end
 
-bash "copy node to warden base" do
+directory node[:vblob][:path] do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
+end
+
+bash "install node js for vblob" do
+  user node[:deployment][:user]
   code <<-EOH
-  cp -r #{node[:node06][:path]}/* #{node[:warden][:rootfs_path]}/usr/
+    mkdir -p #{node[:vblob][:path]}/node
+    cp -r #{node[:node06][:path]}/* #{node[:vblob][:path]}/node/
+  EOH
+  not_if do
+    ::File.exists?(File.join(node[:vblob][:path], "node", "bin", "node"))
+  end
+end
+
+bash "install vblob tools" do
+  user node[:deployment][:user]
+  code <<-EOH
+    mkdir -p #{node[:vblob][:path]}/common/bin
+    cp #{node[:service][:common_path]}/utils.sh #{node[:vblob][:path]}/common/bin
   EOH
 end
 
-template "vblob_startup.sh" do
-   path File.join(node[:warden][:rootfs_path], "usr", "bin", "vblob_startup.sh")
-   source "vblob_startup.sh.erb"
+template File.join(node[:vblob][:path], "common", "bin", "warden_service_ctl") do
+   source "warden_service_ctl.erb"
    mode 0755
 end
 
 node[:vblob][:supported_versions].each do |version|
   bash "install vblob version #{version}" do
+    user node[:deployment][:user]
     code <<-EOH
-    mkdir -p #{File.join(node[:warden][:rootfs_path], node[:vblob][:path_in_warden], version)}
-    cd /tmp
-    git reset --hard #{node[:vblob][:commit][version]}
-    cp -af vblob/* #{File.join(node[:warden][:rootfs_path], node[:vblob][:path_in_warden], version)}
+      mkdir -p #{File.join(node[:vblob][:path], version)}
+      cd /tmp/vblob
+      git reset --hard #{node[:vblob][:commit][version]}
+      cp -af * #{File.join(node[:vblob][:path], version)}
     EOH
     not_if do
-      ::File.exists?(File.join(node[:warden][:rootfs_path], node[:vblob][:path_in_warden], version, "server.js"))
+      ::File.exists?(File.join(node[:vblob][:path], version, "server.js"))
     end
   end
 end
