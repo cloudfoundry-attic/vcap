16409d2059b38c2a6be1e3757c2508552f8d4b52
Haipeng Wu <hwu@rbcon.com>
2012-12-24 00:44:17 -0700

    Upgrade postgresql to latest in services_ng branch
    Change-Id: I4fd804c2232a8b01b40df8e0284913cf2254e80e

--- dev_setup/cookbooks/postgresql/recipes/service.rb
+++ dev_setup/cookbooks/postgresql/recipes/service.rb
@@ -6,18 +6,109 @@
 #
 #
 
-case node['platform']
-when "ubuntu"
+version = node[:postgresql][:default_version]
 
-  /\s*\d*.\d*\s*/ =~ "#{node[:postgresql][:service_version]}"
-  pg_major_version = $&.strip
-  pg_port = "#{node[:postgresql][:service_port]}"
-  cf_pg_install(pg_major_version, pg_port)
-
-else
-    Chef::Log.error("Installation of PostgreSQL is not supported on this platform.")
+postgresql_package = File.join(node[:deployment][:setup_cache], "postgresql-#{version}-x86_64.tar.gz")
+cf_remote_file postgresql_package do
+  owner node[:deployment][:user]
+  id node[:postgresql_node][:id][:package]["#{version}"]
+  checksum node[:postgresql_node][:checksum][:package]["#{version}"]
 end
 
-cf_pg_update_hba_conf(node[:postgresql_node][:database], node[:postgresql][:server_root_user], node[:postgresql][:service_version])
-cf_pg_hba_local_trust(node[:postgresql][:service_version])
-cf_pg_setup_db(node[:postgresql_node][:database], node[:postgresql][:server_root_user], node[:postgresql][:server_root_password], true, node[:postgresql][:service_port])
+postgresql_initdb = File.join(node[:deployment][:setup_cache], "postgresql-initdb-#{version}-x86_64.tar.gz")
+cf_remote_file postgresql_initdb do
+  owner node[:deployment][:user]
+  id node[:postgresql_node][:id][:initdb]["#{version}"]
+  checksum node[:postgresql_node][:checksum][:initdb]["#{version}"]
+end
+
+directory node[:postgresql_node][:path] do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
+end
+
+bash "install postgresql #{version}" do
+  cwd File.join("", "tmp")
+  user node[:deployment][:user]
+  code <<-EOH
+    mkdir "postgresql-#{version}-x86_64"
+    mkdir -p "#{node[:postgresql_node][:path]}/#{version}"
+    cd "postgresql-#{version}-x86_64"
+    tar zxvf #{postgresql_package}
+    cp -a * "#{node[:postgresql_node][:path]}/#{version}"
+  EOH
+  not_if do
+    ::File.exists?(File.join("#{node[:postgresql_node][:path]}", "#{version}", "bin", "postgres"))
+  end
+end
+
+initdb_dir = File.join(node[:postgresql_node][:path], node[:postgresql][:default_version], "initdb")
+bash "install initdb" do
+  cwd File.join("", "tmp")
+  user node[:deployment][:user]
+  code <<-EOH
+    tar zxvf #{postgresql_initdb}
+    cp -r initdb "#{initdb_dir}"
+    mkdir -p "#{initdb_dir}/pg_log"
+    chmod -R 755 "#{initdb_dir}"
+  EOH
+  not_if do
+    ::File.exists?(File.join("#{node[:postgresql_node][:path]}", "#{node[:postgresql][:default_version]}", "initdb"))
+  end
+end
+
+template File.join(initdb_dir, "postgresql.conf") do
+   source "postgresql91.conf.erb"
+   owner node[:deployment][:user]
+   mode 0755
+end
+
+directory File.join(node[:postgresql_node][:path], "common", "bin") do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
+end
+
+directory File.join(node[:postgresql_node][:path], "common", "config") do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
+end
+
+bash "install postgresql tools" do
+  user node[:deployment][:user]
+  code <<-EOH
+    cp #{node[:service][:common_path]}/utils.sh #{node[:postgresql_node][:path]}/common/bin
+  EOH
+end
+
+template File.join(node[:postgresql_node][:path], "common", "bin", "warden_service_ctl") do
+   source "warden_service_ctl.erb"
+   owner node[:deployment][:user]
+   mode 0755
+end
+
+template File.join(node[:postgresql_node][:path], "common", "bin", "pre_service_start.sh") do
+   source "pre_service_start.sh.erb"
+   owner node[:deployment][:user]
+   mode 0755
+end
+
+template File.join(node[:postgresql_node][:path], "common", "bin", "post_service_start.sh") do
+   source "post_service_start.sh.erb"
+   owner node[:deployment][:user]
+   mode 0755
+end
+
+template File.join(node[:postgresql_node][:path], "common", "bin", "postgresql_ctl") do
+   source "postgresql_ctl.erb"
+   owner node[:deployment][:user]
+   mode 0755
+end
