9426fc7a09ece4eee4ebec83231fc74d16c6b68a
Haipeng Wu <hwu@rbcon.com>
2012-12-25 23:22:29 -0700

    Upgrade rabbitmq to latest in services_ng branch
    Change-Id: I20d298c0047542c75ae7def445714dfa0fb2115e

--- dev_setup/cookbooks/rabbitmq/recipes/default.rb
+++ dev_setup/cookbooks/rabbitmq/recipes/default.rb
@@ -6,10 +6,13 @@
 #
 #
 
-template "rabbitmq_startup.sh" do
-   path File.join(node[:warden][:rootfs_path], "usr", "bin", "rabbitmq_startup.sh")
-   source "rabbitmq_startup.sh.erb"
-   mode 0755
+# install erlang
+directory "#{node[:rabbitmq][:path]}" do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
 end
 
 rabbitmq_erlang_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-erlang.tar.gz")
@@ -20,13 +23,16 @@ cf_remote_file rabbitmq_erlang_tarball_path do
 end
 
 bash "Install Erlang for RabbitMQ" do
+  user node[:deployment][:user]
   code <<-EOH
-  cd #{node[:warden][:rootfs_path]}/var/vcap/packages
-  tar xzf #{rabbitmq_erlang_tarball_path}
+    cd #{node[:rabbitmq][:path]}
+    tar xzf #{rabbitmq_erlang_tarball_path}
   EOH
 end
 
-directory "#{node[:rabbitmq_node][:proxy_dir]}" do
+# install daylimit
+daylimit_dir = File.join(node[:rabbitmq][:path], "daylimit_ng")
+directory daylimit_dir do
   owner node[:deployment][:user]
   group node[:deployment][:group]
   mode "0755"
@@ -34,37 +40,91 @@ directory "#{node[:rabbitmq_node][:proxy_dir]}" do
   action :create
 end
 
-bandwidth_proxy = File.join(node[:rabbitmq_node][:proxy_dir], "bandwidth_proxy")
-cf_remote_file bandwidth_proxy do
-  owner node[:deployment][:user]
-  id node[:rabbitmq_node][:proxy_id]
-  checksum node[:rabbitmq_node][:proxy_checksum]
-  mode "0755"
+daylimit_src = File.join(node["cloudfoundry"]["path"], "services", "tools", "daylimit_ng")
+bash "install daylimit_ng for RabbitMQ" do
+  user node[:deployment][:user]
+  code <<-EOH
+    git clone -q #{node[:rabbitmq_node][:govendor_repo]} /tmp/govendor
+
+    mkdir -p #{daylimit_dir}/src/daylimit_ng
+    cp -rf #{daylimit_src}/* #{daylimit_dir}/src/daylimit_ng
+    PATH=#{node[:go][:path]}/bin:$PATH
+    export GOROOT=#{node[:go][:path]}
+    export GOPATH=#{daylimit_dir}:/tmp/govendor
+
+    cd #{daylimit_dir}/src
+    go install daylimit_ng
+  EOH
+  not_if do
+    ::File.exists?(File.join("#{daylimit_dir}", "bin", "daylimit_ng"))
+  end
 end
 
+# service daylimit_ng start
+#template "daylimit_ng.conf" do
+#  path File.join(node[:deployment][:config_path], "daylimit_ng.conf")
+#  source "daylimit_ng.conf.erb"
+#  owner node[:deployment][:user]
+#  mode 0644
+#end
+
+#template "/etc/init.d/daylimit_ng" do
+#  path File.join("", "etc", "init.d", "daylimit_ng")
+#  source "daylimit_ng.erb"
+#  mode 0755
+#end
+
+#service "daylimit_ng" do
+#  supports :status => true, :restart => true, :reload => true
+#  action [ :enable, :restart ]
+#end
+
+# install rabbitmq
 node[:rabbitmq][:supported_versions].each do |version, install_version|
   #TODO, need more refine to actually support mutiple versions
   Chef::Log.info("Building rabbitmq version: #{version} - #{install_version}")
 
   case node['platform']
   when "ubuntu"
+    source_file_id, source_file_checksum = id_and_checksum_for_rabbitmq_version(install_version)
     rabbitmq_tarball_path = File.join(node[:deployment][:setup_cache], "rabbitmq-server-with-plugins-#{install_version}.tar.gz")
     cf_remote_file rabbitmq_tarball_path do
       owner node[:deployment][:user]
-      id node[:rabbitmq][:id]["#{install_version}"]
-      checksum node[:rabbitmq][:checksum]["#{install_version}"]
+      id source_file_id
+      checksum source_file_checksum
     end
 
-    bash "Install RabbitMQ" do
+    bash "Install RabbitMQ #{install_version} As #{version}" do
       cwd File.join("", "tmp")
       code <<-EOH
-      tar xzf #{rabbitmq_tarball_path}
-      mkdir -p  #{node[:warden][:rootfs_path]}/var/vcap/packages/rabbitmq/#{version}
-      cp -rf rabbitmq/* #{node[:warden][:rootfs_path]}/var/vcap/packages/rabbitmq/#{version}
+        tar xzf #{rabbitmq_tarball_path}
+        mkdir -p #{node[:rabbitmq][:path]}/#{version}
+        cp -rf rabbitmq/* #{node[:rabbitmq][:path]}/#{version}
       EOH
     end
 
   else
     Chef::Log.error("Installation of rabbitmq packages not supported on this platform.")
   end
+end
+
+bin_dir = File.join(node[:rabbitmq][:path], "common", "bin")
+directory bin_dir do
+  owner node[:deployment][:user]
+  group node[:deployment][:group]
+  mode "0755"
+  recursive true
+  action :create
+end
+
+bash "install rabbitmq tools" do
+  user node[:deployment][:user]
+  code <<-EOH
+    cp #{node[:service][:common_path]}/utils.sh #{bin_dir}
+  EOH
+end
+
+template File.join(bin_dir, "warden_service_ctl") do
+   source "warden_service_ctl.erb"
+   mode 0755
 end
