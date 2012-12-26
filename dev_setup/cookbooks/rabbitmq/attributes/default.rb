f6ac8bb122a0a9d5026f9d200255f1fe5f1cc7ad
Haipeng Wu <hwu@rbcon.com>
2012-12-25 23:22:29 -0700

    Upgrade rabbitmq to latest in services_ng branch
    Change-Id: I20d298c0047542c75ae7def445714dfa0fb2115e

--- dev_setup/cookbooks/rabbitmq/attributes/default.rb
+++ dev_setup/cookbooks/rabbitmq/attributes/default.rb
@@ -1,31 +1,36 @@
 include_attribute "deployment"
+include_attribute "service"
 
 default[:rabbitmq][:supported_versions] = {
         "2.4" => "2.4.1",
+        "2.8" => "2.8.7",
 }
 default[:rabbitmq][:version_aliases] = {
-        "current" => "2.4",
+        "deprecated" => "2.8",
+        "current"    => "2.4",
 }
-default[:rabbitmq][:default_version] = "2.4"
+default[:rabbitmq][:default_version] = "2.8"
 
-default[:rabbitmq][:path] = File.join(node[:deployment][:home], "deploy", "rabbitmq")
+default[:rabbitmq][:path] = File.join(node[:service][:path], "rabbitmq")
 default[:rabbitmq][:id] = {
-  "2.4.1" => "eyJzaWciOiJVOUo4bW9Kci9tSlY2VW84WG9sa1NVMEFMb0k9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMTFlMTIxMDA0ZTRlN2Q1MTFmODIxMDUwNWE5N2EwZjI4MDUi%0AfQ==%0A"
+  "2.4.1" => "eyJzaWciOiJOTWRVQm01RHU3c1RXcHF4dUovUm93S1c0UUU9Iiwib2lkIjoi%0ANGU0ZTc4YmNhNDFlMTIxMjA0ZTRlODZlZTUzOTIxMDUwMTlmYTU0YWRkZTEi%0AfQ==%0A",
+  "2.8.7" => "eyJvaWQiOiI0ZTRlNzhiY2E0MWUxMjEwMDRlNGU3ZDUxNzYxOGYwNTBiMzI2%0ANzM2ODZlZCIsInNpZyI6IjkwaDNnYnRuUzJKTXhSZmpVSlZxanUwT1RRYz0i%0AfQ==%0A",
 }
+
 default[:rabbitmq][:checksum] = {
-  "2.4.1" => "1cc8dbe3a54b7ef454adb3b6f3bce2f4c798e45ab1608b9600b2dc01a04e5858"
+  "2.4.1" => "0a87dfe4489b0ddabfee7306536530934a4d4518ef0821e08634c7a07d4cf732",
+  "2.8.7" => "7a177c541ad6a33d639330d09503d0948e77208323bd8e603c48e40cd041a432",
 }
 
 default[:rabbitmq][:erlang_id] = "eyJzaWciOiJIb2RpNmdVdnp0S2VHSGRtRUxYQ3dGWnc3WHM9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMzFlMTIxMDA0ZTRlN2Q1MTQ3NDVmMDUwNTljZDgxMmY5YWMi%0AfQ==%0A"
 default[:rabbitmq][:erlang_checksum] = "4c72446596803e8a41ec06e7bee5e840d81756f25613bbe3042609c7d0cb0d6b"
 
-default[:rabbitmq_gateway][:service][:timeout] = "15"
-default[:rabbitmq_gateway][:node_timeout] = "10"
+default[:rabbitmq_gateway][:service][:timeout] = "25"
+default[:rabbitmq_gateway][:node_timeout] = "20"
 
 default[:rabbitmq_node][:index] = "0"
 default[:rabbitmq_node][:token] = "changerabbitmqtoken"
-default[:rabbitmq_node][:op_time_limit] = "6"
-default[:rabbitmq_node][:rabbitmq_start_timeout] = "5"
-default[:rabbitmq_node][:proxy_dir] = File.join(node[:deployment][:home], "deploy", "bandwidth_proxy", "bin")
-default[:rabbitmq_node][:proxy_id] = "eyJzaWciOiJpQXZ5aUYyZWVVZmEyU3RXUW82UVIveHZxRWM9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMTFlMTIyMjA0ZTRlOTg2MzhiNzYzMDUwYjcxMzk2YjVmNzIi%0AfQ==%0A"
-default[:rabbitmq_node][:proxy_checksum] = "2b17a3b9ab3142efaf731d1e6adbe338cdcc9651667b17af93bbaa3684d605f7"
+default[:rabbitmq_node][:op_time_limit] = "15"
+default[:rabbitmq_node][:rabbitmq_start_timeout] = "10"
+
+default[:rabbitmq_node][:govendor_repo] = "git://github.com/cloudfoundry/govendor.git"
